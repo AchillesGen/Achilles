faef8120cc6058d71895bbcbbcff0fe384f815aa
Joshua Isaacson <isaacson@fnal.gov>
2018-10-23 21:10:36 -0500

    Begin getting data for flux information for the neutrino experiments. Also begin testing whether it is better to use histogram for the cdf or to generate a kde to use for the cdf

---
+++ nuChic/flux.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy.stats import gaussian_kde
+import pandas as pd
+
+def generate(pdf, x_grid, nevents=10000):
+    cdf = np.cumsum(pdf)
+    cdf = cdf / cdf[-1]
+    values = np.random.rand(int(nevents))
+    value_bins = np.searchsorted(cdf, values)
+    random_from_cdf = x_grid[value_bins]
+    return random_from_cdf
+
+def kde(x, x_grid, bandwidth=0.2, **kwargs):
+    kde = gaussian_kde(x, bw_method=bandwidth / x.std(ddof=1), **kwargs)
+    return kde.evaluate(x_grid)
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    data = pd.read_csv('../flux/t2k_flux.csv',index_col=0)
+    x_grid = data['low'].values
+    integral = np.sum(data['numu_flux'].values)
+    random_from_cdf = generate(data['numu_flux'].values/integral, x_grid,1E7)
+
+    kdepdf = kde(random_from_cdf,x_grid,bandwidth=0.1)
+    random_from_kde = generate(kdepdf, x_grid, 1E7)
+
+    plt.subplot(121)
+    plt.plot(data['low'].values,data['numu_flux'].values/integral/0.05,ls='steps', label='hist')
+    plt.plot(x_grid, kdepdf, color='r')
+    plt.yscale('log')
+    plt.ylim(1E-8,5)
+    plt.subplot(122)
+    plt.hist(random_from_cdf, len(x_grid))
+    plt.hist(random_from_kde, len(x_grid))
+    plt.yscale('log')
+    plt.show()
