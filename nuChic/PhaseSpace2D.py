e17cfe7b57e5fdefcc9baa0ecd1c919032367e48
Joshua Isaacson <isaacson@fnal.gov>
2018-10-25 13:55:27 -0500

    Add comments for the 2 body Phase Space

--- nuChic/PhaseSpace2D.py
+++ nuChic/PhaseSpace2D.py
@@ -41,6 +41,7 @@ class PhaseSpace:
         E2 = self._beam2
 
 
+        # Load the mass into these variables for making the equations simplier
         m1 = self._mass[0]
         m2 = self._mass[1]
         m3 = self._mass[2]
@@ -53,13 +54,21 @@ class PhaseSpace:
        
         # Boost to center of mass frame
         self._betaCM = pLab.BoostVector()
-        p1 = p1.Boost(self._betaCM)
-        p2 = p2.Boost(self._betaCM)
+        p1 = p1.Boost(-self._betaCM)
+        p2 = p2.Boost(-self._betaCM)
+
+        # Calculate the momentum of the outgoing particles
         Ecm = p1.E + p2.E
         if Ecm < m3+m3:
             return 0, None
+        # E_CM = E_3 + E_4
+        # E_3^2 = p_CM^2 + m_3^2
+        # E_4^2 = p_CM^2 + m_4^2
+        # Solve for p_CM^2: p_CM^2 = (E_CM-m_3-m_4)*(E_CM+m_3-m_4)*(E_CM-m_3+m_4)*(E_CM+m_3+m_4)/(2*E_CM)^2
         pCM = (Ecm-m3-m4)*(Ecm+m3-m4)*(Ecm-m3+m4)*(Ecm+m3+m4)
         pCM = np.sqrt(pCM)/(2*Ecm)        
+
+        # Fil the momentum
         E3 = np.sqrt(pCM**2+m3**2)
         E4 = np.sqrt(pCM**2+m4**2)
         p3 = Vec4(E3,
@@ -70,8 +79,9 @@ class PhaseSpace:
                   -pCM*np.sin(theta)*np.cos(phi),
                   -pCM*np.sin(theta)*np.sin(phi),
                   -pCM*np.cos(theta))
-
         moms = [p1,p2,p3,p4]
+
+        # Calculate the phase space weight
         wgt = 1.0/(16.0*np.pi**2*Ecm**2)*p1.pz/pCM*dphi*dcos_theta
 
         return wgt, moms
@@ -81,6 +91,7 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import argparse
 
+    # Arguments to be changed on the command line
     parser = argparse.ArgumentParser()
     parser.add_argument('--E1', default=100, type=float, help='Energy of first beam')
     parser.add_argument('--E2', default=100, type=float, help='Energy of second beam')
@@ -90,21 +101,26 @@ if __name__ == '__main__':
     parser.add_argument('--m4', default=0, type=float, help='Mass of fourth particle')
     parser.add_argument('--nevents', default=10000, type=int, help='Number of events')
 
+    # Parse input arguments
     args = vars(parser.parse_args())
     E1 = args['E1']
     E2 = args['E2']
     mass = [args['m1'],args['m2'],args['m3'],args['m4']]
+    nevents = args['nevents']
 
+    # Arrays to hold the histograms and the weight of each event
     s = []
-    Q2Vals = []
     costheta = []
     phi = []
     wgts = []
-    ps = PhaseSpace(E1,E2,4,mass)
-    nevents = args['nevents']
 
+    # Initialize the phase space class
+    ps = PhaseSpace(E1,E2,4,mass)
+
+    # hbarc2 = 3.8937966 * 10^8 pb*GeV^2 (Convert units from 1/GeV^2 to pb)
     hbarc2 = 3.8937966E8
 
+    # Define the matrix element for e+ e- -> mu+ mu- for debugging purposes
     def Mat(moms,mass):
         s = (moms[0]+moms[1]).dot(moms[0]+moms[1])
         t = (moms[0]-moms[2]).dot(moms[0]-moms[2])
@@ -112,41 +128,44 @@ if __name__ == '__main__':
 
         return 2*(2*np.pi*1.0/137.0)**2*(t**2+u**2+4*s*(mass[0]**2+mass[2]**2)-2*(mass[0]**2+mass[2]**2)**2)/s**2
 
+    # Generate an event given an input from VEGAS
     def GenerateEvent(x):
         wgt, event = ps.Generate2Body(x)
         if wgt == 0:
             return 0
 
         wgt *= Mat(event,ps._mass)*hbarc2
-
+    
+        # If on the main run, fill the histograms
         if fill:
             s.append((event[2]+event[3]).dot(event[2]+event[3]))
-            Q2 = (event[0]-event[2]).dot(event[0]-event[2])
-            Q2Vals.append(Q2)
-
             costheta.append(np.cos(event[2].Theta()))
             phi.append(event[2].Phi())
-
+    
+            # Weights need to be normalized to the number of events to ensure the total cross-section is correct in the plots
             wgts.append(wgt/nevents)
 
         return wgt
 
+    # Initialize VEGAS
     integ = vegas.Integrator([[0,1],[0,1]]) 
 
     # Preliminary run
     fill = False
     integ(GenerateEvent,nitn=20,neval=1e5)
 
+    # Main run (fill histograms)
     fill = True
     result = integ(GenerateEvent,nitn=10,neval=nevents/10)
 
+    # Print a summary of the VEGAS Integration results
     print(result.summary())
 
+    # Plot the diagnostic histograms
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2)
     ax1.hist(wgts)
     ax2.hist(costheta,weights=wgts, bins=100)
     ax3.hist(phi,weights=wgts)
     ax4.hist(s,weights=wgts)
-
     plt.tight_layout()
     plt.show()
