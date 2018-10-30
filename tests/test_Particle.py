dc44a4d2ae4549ee77151712ec99467ad8488137
Joshua Isaacson <isaacson@fnal.gov>
2018-10-30 15:23:14 -0500

    Fix tests to make them pass

--- tests/test_Particle.py
+++ tests/test_Particle.py
@@ -5,20 +5,23 @@ import numpy as np
 import pytest
 
 def test_Particle_init():
-    part = Particle(2212,Vec4(4,3,2,1),1,[],[])
+    part = Particle(2212,Vec4(4,3,2,1),1,1,1,None,None)
     assert part.pid == 2212
     assert part.mom == Vec4(4,3,2,1)
-    assert part.mothers == []
-    assert part.daughters == []
+    assert part.charge == 1
+    assert part.I3 == 1
+    assert part.status == 1
+    assert part.mothers == None
+    assert part.daughters == None
 
 def test_Particle_isFinal():
-    part = Particle(2212,Vec4(4,3,2,1),1,[],[])
+    part = Particle(2212,Vec4(4,3,2,1),status=1)
     assert part.isFinal()
     part.status = 0
     assert not part.isFinal()
 
 def test_particle_Mom():
-    part = Particle(2212,Vec4(4,3,2,1),1,[],[])
+    part = Particle(2212,Vec4(4,3,2,1),1,None,None)
     assert part.E() == 4
     assert part.Px() == 3
     assert part.Py() == 2
