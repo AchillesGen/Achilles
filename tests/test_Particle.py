e187ade6cbd68916659c2beb296f8e1a0d96e023
Joshua Isaacson <isaacson@fnal.gov>
2018-12-03 11:48:48 -0600

    Fix particle test to address the new initialization to include position

--- tests/test_Particle.py
+++ tests/test_Particle.py
@@ -5,12 +5,11 @@ import numpy as np
 import pytest
 
 def test_Particle_init():
-    part = Particle(2212,Vec4(4,3,2,1),1,1,1,None,None)
+    part = Particle(2212,Vec4(4,3,2,1),Vec3(0,0,0),0,None,None)
     assert part.pid == 2212
     assert part.mom == Vec4(4,3,2,1)
-    assert part.charge == 1
-    assert part.I3 == 1
-    assert part.status == 1
+    assert part.pos == Vec3(0,0,0)
+    assert part.status == 0
     assert part.mothers == []
     assert part.daughters == []
 
