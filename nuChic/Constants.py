3f4cb1d6e2610d3cfbe44c09590a36119ae52abd
Joshua Isaacson <isaacson@fnal.gov>
2018-12-14 14:43:40 -0600

    Add in changes to the particle class, along with the jupyter notebook for the cascade 	new file:   CascadeTesting.ipynb 	new file:   ../densities/c12.density 	new file:   ../nuChic/Constants.py 	modified:   ../nuChic/Particle.py

---
+++ nuChic/Constants.py
@@ -0,0 +1,6 @@
+GeV = 1.0
+fm = 1.0
+m = 1e12 * fm
+MeV = 1.0/1000.0 * GeV
+c = 3e8 * m 
+hbarc = 200 * MeV * fm
--- nuChic/Particle.py
+++ nuChic/Particle.py
@@ -1,4 +1,5 @@
 import numpy as np
+from nuChic.Constants import hbarc
 from nuChic.FourVector import Vec4
 from nuChic.ThreeVector import Vec3
 
@@ -35,26 +36,23 @@ class Particle:
     def r(self):
         return self.pos.P()
 
-    def propagate(self,path,time):
-        # Propagation of a particle with mean free path=path with a time step(time)
-        dist = self.vec.P()*time
-
-        if path < dist:
-            prop_dist = path
-        else:
-            prop_dist = dist
+    def propagate(self,time):
+        # Propagation of a particle for a given time step
+        # The velocity is in terms beta, time is in units of GeV^-1
+        # To get distance need to multiply by hbar*c
+        dist = self.mom.P()/self.mom.E*time*hbarc
 
         theta = self.vec.Theta()
         phi = self.vec.Phi()
 
-        prop_dist_x = prop_dist*np.sin(theta)*np.cos(phi)
-        prop_dist_y = prop_dist*np.sin(theta)*np.sin(phi)
-        prop_dist_z = prop_dist*np.cos(theta)
+        prop_dist_x = dist*np.sin(theta)*np.cos(phi)
+        prop_dist_y = dist*np.sin(theta)*np.sin(phi)
+        prop_dist_z = dist*np.cos(theta)
 
         self.prop_dist = Vec3(prop_dist_x,prop_dist_y,prop_dist_z)
         self.pos += self.prop_dist
 
-        return prop_dist
+        return self.prop_dist
 
     def back_propagate(self):
         self.pos -= self.prop_dist
