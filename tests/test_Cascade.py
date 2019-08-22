a84de9417974fe410be75362f4fb5d0208ef6200
Pedro Machado <pmachado@fnal.gov>
2019-08-22 10:55:50 -0500

    added cascade tests

--- tests/test_Cascade.py
+++ tests/test_Cascade.py
@@ -1,7 +1,64 @@
 from nuChic.Cascade import FSI
 from nuChic.Nucleus import Nucleus
+from nuChic.Constants import MeV, fm, mN
+from nuChic.FourVector import Vec4 
+from nuChic.ThreeVector import Vec3
+from nuChic.Particle import Particle
 import numpy as np
+from numpy import linalg as LA
 import pytest
 
+nuc = Nucleus(6,12,10,225)
+energy_transfer=500*MeV
+dt=1*fm
+cascade = FSI(nuc, energy_transfer, dt)
+cascade_b = FSI(nuc, energy_transfer, dt)
+
 def test_Cascade_init():
-    pass
+    assert len(cascade.kicked_idxs) == 1
+    print(type(cascade.nucleons[cascade.kicked_idxs[0]]))
+    print(isinstance(cascade.nucleons[cascade.kicked_idxs[0]],Vec4))
+    print(cascade.nucleons[cascade.kicked_idxs[0]].M())
+#    assert cascade.nucleons[cascade.kicked_idxs[0]].mom.M()==mN
+    assert len(cascade.outgoing_particles) == 0
+    number_of_protons = 0
+    number_of_neutrons = 0
+    for i in range(len(cascade.nucleons)) :
+        if cascade.nucleons[i].pid == 2212 :
+            number_of_protons += 1
+        if cascade.nucleons[i].pid == 2212 :
+            number_of_neutrons += 1
+    assert number_of_protons == 6 and number_of_neutrons == 6
+
+def test_points_in_cylinder():    
+    assert cascade.points_in_cylinder(pt1=[0,0,0], pt2=[0,0,1], r=1, q=[0.2,0.2,0.2])
+    assert not(cascade.points_in_cylinder(pt1=[0,0,0], pt2=[0,0,1], r=1, q=[0,1.1,0.5]))
+    
+def test_to_cartesian():
+    coords = [[1,0,0], [1,np.pi/2,0], [1,np.pi/2,np.pi/2]]
+    results = [[0,0,1], [0,1,0], [1,0,0]]
+    for i in range(3):
+        assert LA.norm((cascade.to_cartesian(coords[i])-results[i])) < 1E-10
+        
+def test_interacted():
+    Ep = energy_transfer # massless proton for easy propagation
+    pp = np.sqrt(Ep**2)
+
+    positions = [Vec3(100,100,100), Vec3(0,0,dt/2), Vec3(0,0,1.5*dt), Vec3(0,0.5,0.5*dt), Vec3(0,1.5,0.5*dt)]
+    tests = [False, True, False, True, False]
+    for i in range(len(positions)) :
+        # Put all other nucleaons everyone far
+        cascade_b.nucleons = [Particle(pid=2212, mom=Vec4(mN,0,0,0), pos=Vec3(100,100,100)) for i in range(len(cascade.nucleons))]
+        # Kicked particle
+        cascade_b.nucleons[0] = Particle(pid=2212, mom=Vec4(Ep,0,0,pp), pos=Vec3(0,0,0))
+        cascade_b.kicked_idxs = []
+        cascade_b.kicked_idxs.append(0)
+        cascade_b.nucleons[cascade_b.kicked_idxs[0]].status = -1 # propagating nucleon
+        cascade_b.nucleons[10] = Particle(pid=2212, mom=Vec4(mN,0,0,0), pos=positions[i])
+        print(cascade_b.nucleons[0].pos, cascade_b.nucleons[10].pos)
+        in_cylinder, idx = cascade_b.interacted(0, sigma=1)
+#        assert in_cylinder==tests[i]
+        print(in_cylinder, tests[i], cascade_b.nucleons[0].pos, cascade_b.nucleons[10].pos)
+    
+test_Cascade_init()
+#test_interacted()
