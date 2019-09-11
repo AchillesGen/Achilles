7477be6994147b4c6bb9cde357b1574ccdc8edcc
Joshua Isaacson <isaacson@fnal.gov>
2019-09-11 16:16:27 -0500

    Implement test of loading the main program

--- nuChic/main.py
+++ nuChic/main.py
@@ -9,10 +9,13 @@ from .inclusive import Quasielastic
 from .Nucleus import Nucleus
 from .Constants import MeV
 from .Cascade import FSI
+from .FourVector import Vec4
+from .utils import momentum_sort
 
 FLAGS = flags.FLAGS
-flags.DEFINE_bool('cascade',True,'Flag to turn on/off the cascade',short_name='c')
-flags.DEFINE_bool('folding',None,'Flag to turn on/off the folding function')
+flags.DEFINE_flag('cascade',None,'Flag to turn on/off the cascade',short_name='c')
+flags.DEFINE_flag('folding',None,'Flag to turn on/off the folding function')
+flags.DEFINE_flag('testing',None,'Flag to test the basics of the program')
 
 DIR, FILE = os.path.split(__file__)
 
@@ -55,7 +58,7 @@ def run():
     
     print(result.summary())
     
-    nitn = 40
+    nitn = 20
     count = 0
     zeros = 0
     zeros_f = 0
@@ -71,6 +74,8 @@ def run():
             if FLAGS.folding:
                 wgt_f = wgt[0]
                 wgt = wgt[1]
+            else:
+                wgt_f = 0
 
             if wgt == 0:
                 zeros += 1
@@ -78,36 +83,37 @@ def run():
             if wgt_f == 0:
                 zeros_f += 1
        
-            omega.append(inclusive.w)
             #mom.append(p_int)
             #energies.append(e_int)
-            #momentum = Vec4(epf*MeV, pf*np.sqrt(sina2)*np.cos(phi)*MeV, pf*np.sqrt(sina2)*np.sin(phi)*MeV, pf*cosa*MeV)
-            #p_E_pre.append(momentum.E)
-            #p_px_pre.append(momentum.px)
-            #p_py_pre.append(momentum.py)
-            #p_pz_pre.append(momentum.pz)
-            wgts.append(wgt*weight/nitn)
-            wgts_f.append(wgt_f*weight/nitn)
+            momentum = inclusive.GenerateMomentum()
+            if momentum is not None:
+                omega.append(inclusive.w)
+                p_E_pre.append(momentum.E)
+                p_px_pre.append(momentum.px)
+                p_py_pre.append(momentum.py)
+                p_pz_pre.append(momentum.pz)
+                wgts.append(wgt*weight/nitn)
+                wgts_f.append(wgt_f*weight/nitn)
    
-            #if FLAGS.cascade:
-            #    fsi.kick(momentum)
-            #    escaped_part = fsi()
-            #    escape.append(len(escaped_part))
-            #    fsi.reset()
+                if FLAGS.cascade:
+                    fsi.kick(momentum)
+                    escaped_part = fsi()
+                    escape.append(len(escaped_part))
+                    fsi.reset()
 
-            #    if len(escaped_part) > 0:
-            #        escaped_part.sort(reverse=True, key=momentumSort)
-            #        momentum = escaped_part[0].mom
-            #        
-            #p_E.append(momentum.E)
-            #p_px.append(momentum.px)
-            #p_py.append(momentum.py)
-            #p_pz.append(momentum.pz)
+                    if len(escaped_part) > 0:
+                        escaped_part.sort(reverse=True, key=momentum_sort)
+                        momentum = escaped_part[0].mom
+                        
+                p_E.append(momentum.E)
+                p_px.append(momentum.px)
+                p_py.append(momentum.py)
+                p_pz.append(momentum.pz)
 
-            #p_E_diff.append(momentum.E-p_E_pre[-1])
-            #p_px_diff.append(momentum.px-p_px_pre[-1])
-            #p_py_diff.append(momentum.py-p_py_pre[-1])
-            #p_pz_diff.append(momentum.pz-p_pz_pre[-1])
+                p_E_diff.append(momentum.E-p_E_pre[-1])
+                p_px_diff.append(momentum.px-p_px_pre[-1])
+                p_py_diff.append(momentum.py-p_py_pre[-1])
+                p_pz_diff.append(momentum.pz-p_pz_pre[-1])
     
     print(sum(wgts),sum(wgts_f))
     print(count)
@@ -153,37 +159,38 @@ def run():
     #ax2.hist(mom,weights=wgts,bins=400)
     #ax3.hist(energies,weights=wgts,bins=400)
     
-#    fig2, axs = plt.subplots(nrows=2, ncols=2)
-#    axs[0][0].hist(p_E,weights=wgts,bins=400)
-#    axs[0][1].hist(p_px,weights=wgts,bins=400)
-#    axs[1][0].hist(p_py,weights=wgts,bins=400)
-#    axs[1][1].hist(p_pz,weights=wgts,bins=400)
-#    fig2.suptitle('After Cascade')
-#
-#    fig3, axs2 = plt.subplots(nrows=2, ncols=2)
-#    axs2[0][0].hist(p_E_pre,weights=wgts,bins=400)
-#    axs2[0][1].hist(p_px_pre,weights=wgts,bins=400)
-#    axs2[1][0].hist(p_py_pre,weights=wgts,bins=400)
-#    axs2[1][1].hist(p_pz_pre,weights=wgts,bins=400)
-#    fig3.suptitle('Before Cascade')
-#
-#    fig4, axs3 = plt.subplots(nrows=2, ncols=2)
-#    axs3[0][0].hist(p_E_diff,weights=wgts,bins=400)
-#    axs3[0][1].hist(p_px_diff,weights=wgts,bins=400)
-#    axs3[1][0].hist(p_py_diff,weights=wgts,bins=400)
-#    axs3[1][1].hist(p_pz_diff,weights=wgts,bins=400)
-#    fig4.suptitle('Difference in momentum')
-#
+    fig2, axs = plt.subplots(nrows=2, ncols=2)
+    axs[0][0].hist(p_E,weights=wgts,bins=400)
+    axs[0][1].hist(p_px,weights=wgts,bins=400)
+    axs[1][0].hist(p_py,weights=wgts,bins=400)
+    axs[1][1].hist(p_pz,weights=wgts,bins=400)
+    fig2.suptitle('After Cascade')
+
+    fig3, axs2 = plt.subplots(nrows=2, ncols=2)
+    axs2[0][0].hist(p_E_pre,weights=wgts,bins=400)
+    axs2[0][1].hist(p_px_pre,weights=wgts,bins=400)
+    axs2[1][0].hist(p_py_pre,weights=wgts,bins=400)
+    axs2[1][1].hist(p_pz_pre,weights=wgts,bins=400)
+    fig3.suptitle('Before Cascade')
+
+    fig4, axs3 = plt.subplots(nrows=2, ncols=2)
+    axs3[0][0].hist(p_E_diff,weights=wgts,bins=400)
+    axs3[0][1].hist(p_px_diff,weights=wgts,bins=400)
+    axs3[1][0].hist(p_py_diff,weights=wgts,bins=400)
+    axs3[1][1].hist(p_pz_diff,weights=wgts,bins=400)
+    fig4.suptitle('Difference in momentum')
+
 #    fig5, ax7 = plt.subplots(nrows=1, ncols=1)
 #    ax7.hist(wgts,bins=np.logspace(np.log10(np.min(wgts)), np.log10(np.max(wgts)), 100))
 #    ax7.set_xscale('log')
 #    ax7.set_yscale('log')
 #
 #    print(np.mean(wgts)/np.max(wgts))
-#   
-#    if FLAGS.cascade:
-#        fig6, ax8 = plt.subplots(nrows=1, ncols=1)
-#        ax8.hist(escape,weights=wgts,bins=np.linspace(0,10,11))
+   
+    if FLAGS.cascade:
+        fig6, ax8 = plt.subplots(nrows=1, ncols=1)
+        ax8.hist(escape,weights=wgts,bins=np.linspace(-0.5,10.5,12))
+        ax8.set_yscale('log')
     
     plt.show()
 
@@ -197,7 +204,10 @@ def main(argv):
     del argv
 
     logging.info('Starting nuChic...')
-    run()
+    if not FLAGS.testing:
+        run()
+
+    logging.info('nuChic finished successfully!')
 
 def nuChic():
     app.run(main)
