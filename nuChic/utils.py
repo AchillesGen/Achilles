0cce2701febb0dad8adba9abc5349552b17947e2
Joshua Isaacson <isaacson@fnal.gov>
2019-09-17 14:56:07 -0500

    Implement to_cartesian in utils.py

--- nuChic/utils.py
+++ nuChic/utils.py
@@ -1,4 +1,20 @@
-from .four_vector import Vec4
+""" Implementation of utility functions for the nuChic code. """
+
+import numpy as np
+
 
 def momentum_sort(elem):
+    """ Sort the array by momentum. """
     return elem.mom.P()
+
+
+def to_cartesian(coords):
+    """Convert spherical coordinates to cartesian coordinates. """
+    # r, theta, phi = coords
+    r = coords[:, 0]
+    theta = coords[:, 1]
+    phi = coords[:, 2]
+    x = r * np.sin(theta) * np.sin(phi)
+    y = r * np.sin(theta) * np.cos(phi)
+    z = r * np.cos(theta)
+    return np.transpose(np.array([x, y, z]))
