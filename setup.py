36d1a111dae6bec5208fd2225a942e1ee73b4c30
Joshua Isaacson <isaacson@fnal.gov>
2019-12-30 15:15:23 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,24 @@
 """ Setup script for the nuchic neutrino event generator. """
 
 # from setuptools import setup, find_packages, Extension
-from os import path
-from io import open
+import io
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
 import setuptools
 from numpy.distutils.core import setup, Extension
-# io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines,
-# and accepts an argument to specify the text encoding
+
+
+def read(*names, **kwargs):
+    """ Read files. """
+    with io.open(
+            join(dirname(__file__), *names),
+            encoding=kwargs.get('encoding', 'utf8')
+    ) as fhs:
+        return fhs.read()
+
 
 EXT1 = Extension(name='xsec', sources=['src/nuchic/xsec.pyf',
                                        'src/nuchic/currents_opt_v1.f90',
@@ -16,17 +27,11 @@ EXT1 = Extension(name='xsec', sources=['src/nuchic/xsec.pyf',
                                        'src/nuchic/nform.f90',
                                        'src/nuchic/xsec.f90'])
 
-HERE = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
-
 setup(
     name='nuchic',
     version='1.0',
     description='Neutrino Event Generator',
-    long_description=LONG_DESCRIPTION,
+    long_description=read('README.md'),
     long_description_content_type='text/markdown',
     url='https://github.com/jxi24/FNALNeuGen',
     author='Joshua Isaacson, \
@@ -47,7 +52,11 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
     ],
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
     python_requires='>=3.5, <4',
     install_requires=[
         'numpy',
