1b98dec2d03ccfb70167bd4a51f0f4d781bf5bf7
Joshua Isaacson <isaacson@fnal.gov>
2020-01-23 14:08:10 -0600

    Remove uneeded files

--- src/nuchic/nucleus.cc
+++
@@ -1,9 +0,0 @@
-#include "../../include/nuchic/nucleus.hh"
-
-using namespace nuchic;
-
-Nucleus::Nucleus(const int& Z, const int& A, const double& binding_, const double& fermiEnergy_, const ConfigMode& mode) :
-    nProtons(Z), nNucleons(A), binding(binding_), fermiEnergy(fermiEnergy_) {
-
-    density = make_density(Z, A, mode) 
-}
