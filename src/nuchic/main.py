1aa96d49f8d47666254ecc776e065500a2dd5cfa
Joshua Isaacson <isaacson@fnal.gov>
2020-01-27 12:18:50 -0600

    Update main driver code for event generation

--- src/nuchic/main.py
+++ src/nuchic/main.py
@@ -10,18 +10,23 @@ class NuChic:
     """ Main driver class for the nuchic code. """
     def __init__(self, run_card):
         settings().load(run_card)
-        self.calc = run_modes.RunMode(settings().get_run('mode', 'generate'), 'test')
         self.nevents = settings().nevents
+        self.calc = run_modes.RunMode(settings().get_run('mode', 'generate'))
 
     def run(self):
         """ Generate events for the given input settings. """
+        events = []
         for _ in tqdm(range(self.nevents), ncols=80):
             event = self.calc.generate_one_event()
+            events.append(event)
+
+        self.calc.finalize(events)
 
 
 def nuchic():
     """ External entry point. """
-    NuChic('run.yml')
+    driver = NuChic('run.yml')
+    driver.run()
 
 
 if __name__ == '__main__':
