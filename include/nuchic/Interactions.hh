97bc37940209a7fde1e4c3f0fe0e90d4a7611b44
Joshua Isaacson <isaacson@fnal.gov>
2020-02-06 15:59:24 -0600

    Preliminary Interactions documentation

--- include/nuchic/Interactions.hh
+++ include/nuchic/Interactions.hh
@@ -17,16 +17,40 @@ double CrossSectionAngle(bool, const double&, const double&);
 double CrossSectionLab(bool, const double&) noexcept;
 ThreeVector MakeMomentumAngular(bool, const double&, const double&, const std::array<double, 2>&);
 
+/// Base class for implementing interaction models. These interaction models focus on the
+/// interactions that occur between hadrons during the intranuclear cascade. This base class
+/// is **not** to be used to inherit from for modeling the hard interactions between leptons
+/// and the nucleus.
 class Interactions {
     public:
-        // Constructors
-        Interactions() {}
-        Interactions(const std::string& data="") {}
-        virtual ~Interactions() {}
+        /// @name Constructors and Destructors
+        ///@{
 
-        // Functions
+        /// Base class constructor
+        Interactions() {}
+
+        /// Base class constructor for classes that need data files
+        Interactions(const std::string& data="") {}
+
+        /// Default destructor
+        virtual ~Interactions() {}
+        ///@}
+
+        /// Determines if a subclass has been registered to InteractionFactory
+        ///@return bool: True if registered, otherwise False
         virtual bool IsRegistered() const noexcept = 0;
+
+        /// Function to determine the cross-section between two particles
+        ///@param part1: The first particle involved with the interaction
+        ///@param part2: The second particle involved with the interaction
+        ///@return double: The cross-section
         virtual double CrossSection(const Particle&, const Particle&) const = 0;
+
+        /// Function to generate momentum for the particles after an interaction
+        ///@param samePID: Used to determine if the two particles are the same type
+        ///@param p1CM: The momentum of the first particle in the center of mass frame
+        ///@param pcm: The center of mass momentum
+        ///@param rans: An array containing two random numbers used to generate phase space
         virtual ThreeVector MakeMomentum(bool, const double&, const double&,
                 const std::array<double, 2>&) const = 0;
 
@@ -34,14 +58,34 @@ class Interactions {
         double CrossSectionLab(bool, const double&) const noexcept;
 };
 
+
+/// The InteractionFactory creates a method of generating an interaction model for the 
+/// intranuclear cascade at runtime by a string in the run card. This allows the user to 
+/// test how different interaction models may effect results without having to recompile any
+/// code.
 class InteractionFactory {
     public:
+        /// Typedef for easier coding
         using TCreateMethod = std::unique_ptr<Interactions>(*)();
+
+        /// Member function to hold how to create a given Interaction object
         TCreateMethod CreateFunc;
 
+        /// Used to ensure only one instance of the factory is created
+        ///@return InteractionFactory: An instance of the interaction factory class
         static InteractionFactory& Instance();
 
+        /// Register a new Interactions subclass to the factory
+        ///@param name: The name of the subclass
+        ///@param funcCreate: The Create function of the subclass
+        ///@return bool: True if the class was registered to the factory, False if the
+        ///              registration failed
         bool Register(const std::string&, TCreateMethod);
+
+        /// Create an instance of the desired subclass based on the input string
+        ///@param name: Name of the subclass object to be created
+        ///@return std::shared_ptr<Interactions>: A pointer to the interaction subclass object
+        ///     (**NOTE**: Returns nullptr if name is not registered)
         std::shared_ptr<Interactions> Create(const std::string&);
 
     private:
@@ -53,26 +97,41 @@ class InteractionFactory {
     bool interaction::registered = InteractionFactory::Instance().Register(interaction::GetName(), \
             interaction::Create);
 
+/// Class for implementing an interaction model based on the Geant4 cross-section data. This
+/// interaction model contains information about the angular distribution of pp, pn, and nn
+/// interactions that occur during the intranuclear cascade.
 class GeantInteractions : public Interactions {
     public:
-        // Initialize GeantInteractions class
+        ///@name Constructors and Destructors
+        ///@{
+
+        /// Initialize GeantInteractions class. This loads data from an input file
+        ///@param filename: The location of the Geant4 hdf5 data file
         GeantInteractions(const std::string&);
+
+        /// Generate a GeantInteractions object. This is used in the InteractionFactory.
+        ///@param data: The location of the data file to load containing the Geant4 cross-sections
         static std::unique_ptr<Interactions> Create(const std::string& data) {
             return std::unique_ptr<GeantInteractions>(
                     new GeantInteractions(data));
         }
 
-        // Destructor
+        /// Default Destructor
         ~GeantInteractions() {};
+        ///@}
 
-        // Functions
+        /// Returns the name of the class, used in the InteractionFactory
+        ///@return std::string: The name of the class
         static std::string GetName() {return "GeantInteractions";}
+
+        // These functions are defined in the base class
         bool IsRegistered() const noexcept {return registered;}
         double CrossSection(const Particle&, const Particle&) const;
         ThreeVector MakeMomentum(bool, const double&, const double&,
                 const std::array<double, 2>&) const;
 
     protected:
+        /// Variable to store if the class has been registered to the InteractionFactory or not
         static bool registered;
 
     private:
