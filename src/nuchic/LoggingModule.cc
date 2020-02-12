52dd5bcc3457a5b7032a7aecc88d1087fb7de1ac
Joshua Isaacson <isaacson@fnal.gov>
2020-02-12 11:47:48 -0600

    Implement spdlog as the default logger
    Setup the repository to pull in spdlog for logging. Additionally, create
    a python interface to the needed functionality through pybind11.
    
    TODO:
    - Put the logger and other python functions into a module as submodules
    - Figure out how to pass the arguments to allow the C++ side to format
      the message instead of having to do it in python

---
+++ src/nuchic/LoggingModule.cc
@@ -0,0 +1,48 @@
+#include "pybind11/pybind11.h"
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(logger, m) {
+    m.def("init",  [](const std::string &logname, const std::string &logfile) {
+        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        console_sink->set_level(spdlog::level::warn);
+        console_sink->set_pattern("[%^%l%$] %v");
+
+        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logfile, true); 
+        file_sink->set_level(spdlog::level::trace);
+    
+        spdlog::logger logger(logname, {console_sink, file_sink});
+        auto logger_ptr = std::make_shared<spdlog::logger>(logger);
+        logger_ptr -> set_level(spdlog::level::debug);
+        spdlog::set_default_logger(logger_ptr);
+    });
+
+    py::enum_<spdlog::level::level_enum>(m, "level")
+        .value("trace", spdlog::level::level_enum::trace)
+        .value("debug", spdlog::level::level_enum::debug)
+        .value("info", spdlog::level::level_enum::info)
+        .value("warning", spdlog::level::level_enum::warn)
+        .value("error", spdlog::level::level_enum::err)
+        .value("critical", spdlog::level::level_enum::critical)
+        .value("off", spdlog::level::level_enum::off);
+
+    m.def("set_level", [](const spdlog::level::level_enum& level) {
+        spdlog::set_level(level);
+    });
+    m.def("set_pattern", [](const std::string& pattern) { spdlog::set_pattern(pattern); });
+
+    m.def("log", [](const spdlog::level::level_enum& level, const std::string& message) {
+        spdlog::log(level, message);
+    });
+
+    m.def("trace", [](const std::string &message) { spdlog::trace(message); });
+    m.def("debug", [](const std::string &message) { spdlog::debug(message); });
+    m.def("info", [](const std::string &message) { spdlog::info(message); });
+    m.def("warning", [](const std::string &message) { spdlog::warn(message); });
+    m.def("error", [](const std::string &message) { spdlog::error(message); });
+    m.def("critical", [](const std::string &message) { spdlog::critical(message); });
+}
