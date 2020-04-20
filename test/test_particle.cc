34aebd103c15b190591156607e8628a49a56cb88
Joshua Isaacson <isaacson@fnal.gov>
2020-04-20 17:30:25 -0500

    Implement Particle, Utility, and Vector Tests

---
+++ test/test_particle.cc
@@ -0,0 +1,35 @@
+#include "catch2/catch.hpp"
+
+#include "nuchic/Particle.hh"
+
+TEST_CASE("Particle Initialization", "[Particle]") {
+    SECTION("Constructors") {
+
+    }
+
+    SECTION("Assignment") {
+
+    }
+}
+
+TEST_CASE("Formation Zone", "[Particle]") {
+
+}
+
+TEST_CASE("Properties", "[Particle]") {
+    SECTION("Momentum") {
+
+    }
+
+    SECTION("Position") {
+
+    }
+
+    SECTION("Status") {
+
+    }
+}
+
+TEST_CASE("Propagate", "[Particle]") {
+
+}
