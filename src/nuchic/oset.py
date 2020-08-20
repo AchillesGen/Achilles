2d082164e50976c5f29b1252289c62188cf8f1cd
Joshua Isaacson <isaacson@fnal.gov>
2020-08-20 16:02:33 -0500

    Continue testing Oset model

--- src/nuchic/oset.py
+++ src/nuchic/oset.py
@@ -11,94 +11,133 @@ HBARC = constants.HBARC
 class Oset:
     def __init__(self, Z, N, fstr2 = 0.360*4*np.pi):
         self.chi = (N-Z)/(N+Z)
+        self.Z = Z
+        self.N = N
+        self.A = Z+N
         self.pmat = np.array([[5-4*self.chi, 1-self.chi, 0],
                               [1+self.chi, 4, 1-self.chi],
                               [0, 1+self.chi, 5+4*self.chi]])
         self.m_delta = physics.ParticleInfo(2114).mass()
-        self.m_pi = (physics.ParticleInfo(211).mass()+physics.ParticleInfo(111).mass())/2.0
+        self.m_pi = physics.ParticleInfo(211).mass()
         self.m_n = physics.ParticleInfo(2212).mass()
         self.norm = HBARC**2 # conversion from MeV^-2 to fm^2
         self.coupling = fstr2/self.m_pi**2
-        self.rho0 = 0.16843764
+        self.rho0 = 0.17
+        self.imb0 = 0.035/self.m_pi**4
+        self.ax3 = 1/(12*np.pi)*self.coupling*self.m_n
+        self.ax2 = 4/9*self.coupling
 
-    def pwave(self, costh, mom):
-        sinth = np.sqrt(1-costh**2)
-        k = physics.Vector4(mom*sinth, 0, mom*costh, np.sqrt(self.m_n**2+mom**2))
+    def cq(self, x):
+        return -5.19*x**2 + 15.35*x + 2.06
 
-        pcm4 = ppi.boost(-(k+ppi).boost_vector())
+    def ca2(self, x):
+        return 1.06*x**2 - 6.64*x + 22.66
 
-        # ecm = (k+self.ppi).mass()
-        # pcm = pcm4.p()
-        ecm = np.sqrt(self.m_pi**2 + 2*self.m_n*ppi.energy() + self.m_n**2)
-        pcm = ppi.p()*self.m_n/ecm
-        self.self_energy_calc(self.rho)
+    def ca3(self, x):
+        return -13.46*x**2 + 46.17*x - 20.34
 
-        gamma = 1.0/(12.0*np.pi)*self.coupling*self.m_n/ecm*pcm**3*self.pauli_blocking(self.ppi, ecm, pcm, self.kf, k)
-        prop2 = 1/((ecm - self.m_delta)**2 + (gamma-self.self_energy)**2)
+    def alpha(self, x):
+        return 0.382*x**2 - 1.322*x + 1.466
 
-        pqel = 1/ppi.energy()*2*mom**2/(np.pi**2)/3*self.coupling*pcm**2*prop2*gamma/HBARC
-
-        return  pqel
-
+    def beta(self, x):
+        return -0.038*x**2 + 0.204*x + 0.613
 
     def __call__(self, radius, ppi):
-        self.rho = self.density(radius)
-        self.kf = (3/2*np.pi**2*self.rho)**(1/3)*HBARC
-        self.ppi = ppi
-        result = dblquad(self.pwave, 0, self.kf, -1, 1)
+        rho = self.density(radius)
+        kf = (3*np.pi**2*rho*self.N/self.A)**(1/3)*HBARC
+        ef = np.sqrt(kf**2 + self.m_n**2)
 
-        ecm = np.sqrt(self.m_pi**2 + 2*self.m_n*ppi.energy() + self.m_n**2)
-        xi = (ecm - self.m_n - self.m_pi)/self.m_pi
-        sqel = (0.19753 + 0.06899*xi - 0.01334*xi**2)/self.m_pi**2*self.norm*self.rho*self.ppi.p()/self.ppi.energy()
-        print(result, sqel)
-        return result[0]+sqel
-        
-        # self.pxsec = self.coupling*prop2*pcm**2*2/3
-        # 
-        # # Absorption
-        # abs_pwave = 4.0/9.0*self.pxsec * (-self.self_energy_abs)
-        # imb0 = 0.035/(self.m_pi)**4
-        # abs_swave = 4*np.pi/ppi.energy()*rho**2*(1+ppi.energy()/(2*self.m_n))*imb0*HBARC**5
+        # Absorption
+        wqa = ppi.energy()
+        if(ppi.energy() - ppi.mass() > 0.5*HBARC):
+            wqa = self.m_pi + 0.5*HBARC 
 
-        # # Qel
-        # pqel = self.pxsec/ppi.energy() * 4 * gamma * mom * mom / (2*np.pi)**3 * 4*np.pi * kf / HBARC
-        # print(pqel, mom * mom * (4*np.pi)/(2*np.pi)**3 * kf, rho)
-        # D = -0.03130 + 0.37062*xi - 0.08229*xi**2
-        # B = 0.21972 + 0.06602*xi - 0.01866*xi**2
-        # A = 0.5*(1+D)
-        # C = 0.5*(1-D)
-        # pqel_tot = pqel*(2-self.chi)/3
-        # sqel_tot = sqel*(1-(B-C)*self.chi)
+        abs_swave = 4*np.pi/ppi.p()*(1+wqa/(2*self.m_n))*self.imb0*rho*rho
+        # Convert to fm^-1
+        abs_swave *= HBARC**5
 
-        # prob_factor = rho*ppi.p()/ppi.energy()
-        # return (pqel_tot+sqel_tot)*prob_factor, abs_pwave*prob_factor+abs_swave
+        # Delta kinematics
+        p2 = ppi.p2() + 0.6*kf**2
+        p = np.sqrt(p2)
+        edelta = ppi.energy() + np.sqrt(0.6*kf**2+self.m_n**2)
+        s = edelta*edelta - p2
+        sqrts = np.sqrt(s)
 
-    def self_energy_calc(self, rho):
-        rho_ratio = rho/self.rho0
-        alpha = 0.42 
-        beta = 0.80
-        gamma1 = 1.60
-        CQ = 12.0
-        CA2 = 16.3
-        CA3 = 15.8
-        abs2 = CA2*(rho_ratio)**beta
-        abs3 = CA3*(rho_ratio)**gamma1
-        self.self_energy_abs = - abs2 - abs3
-        self.self_energy = self.self_energy_abs - CQ*(rho_ratio)**alpha
+        # Pion CM
+        wcm = (s-self.m_n**2+self.m_pi**2)/(2*sqrts)
+        pcm2 = wcm*wcm - self.m_pi**2
+        pcm = np.sqrt(pcm2)
 
-    def pauli_blocking(self, ppi, ecm, pcm, kf, k):
-        ef = np.sqrt(kf**2+self.m_n**2)
-        edelta = ppi.energy()+self.m_n
-        edcm = np.sqrt(pcm*pcm + self.m_n**2)
-        mu0 = (edelta*edcm - ef*ecm)/(ppi+k).p()/pcm
+        # Nucleon CM energy
+        en = sqrts - wcm
+
+        mu0 = (edelta*en - ef*sqrts)/(p*pcm)
         if(mu0 < -1):
-            return -1
+            mu0 = -1
         elif(mu0 > 1):
-            return 1
-        return (mu0**3+mu0+2)/4
+            mu0 = 1
+
+        pauli = 0.25*(2+mu0+mu0**3)
+        if(pauli < 0):
+            pauli = 0
+        elif(pauli > 1):
+            pauli = 1
+
+        gamma_free = self.ax3*pcm*pcm2/sqrts
+        gamma = gamma_free*pauli
+        x = (ppi.energy()-ppi.mass())/self.m_pi
+
+        rho_ratio = rho/self.rho0
+        alpha = self.alpha(x)
+        gamma_cq = self.cq(x)*rho_ratio**alpha
+        if(gamma_cq < 0):
+            gamma_cq = 0
+        beta = self.beta(x)
+        gamma_ca2 = self.ca2(x)*rho_ratio**beta
+        if(gamma_ca2 < 0):
+            gamma_ca2 = 0
+        gamma_ca3 = self.ca3(x)*rho_ratio**(2*beta)
+        if(gamma_ca3 < 0):
+            gamma_ca3 = 0
+        gamma_tot = gamma + gamma_cq + gamma_ca2 + gamma_ca3
+        prop2 = 1/((sqrts - self.m_delta)**2 + gamma_tot**2)
+
+        abs_pwave = self.ax2*pcm2/ppi.p()*rho*(gamma_ca2+gamma_ca3)*prop2
+        # Convert to fm^-1
+        abs_pwave *= HBARC**2
+
+        # Quasielastic
+        qel_pwave = self.ax2*pcm2/ppi.p()*rho*gamma*prop2
+        # Convert to fm^-1
+        qel_pwave *= HBARC**2
+
+        pauli_swave = 1 - (p*pcm-edelta*en+ef*sqrts)/(2*p*pcm)
+        if(pauli_swave < 0):
+            pauli_swave = 0
+        elif(pauli_swave > 1):
+            pauli_swave = 1
+        wswave = sqrts
+        if(sqrts > 6.76*HBARC):
+            wswave = 6.76*HBARC
+        xi = (wswave - self.m_n - self.m_pi)/self.m_pi
+        bxi = 0.21972+0.066025*xi-0.018665*xi**2
+        dxi = -0.03130+0.370625*xi-0.08229*xi**2
+        axi = (1+dxi)/2
+        cxi = (1-dxi)/2
+        sigma = 0.19753+0.06899*xi-0.01334*xi*xi
+        # convert sigma to MeV^2
+        sigma = sigma/self.m_pi**2
+        qel_swave = rho*sigma*pauli_swave
+        # Convert to fm^-1
+        qel_swave *= HBARC**2
+
+        quasielastic = qel_swave + qel_pwave
+        absorption = abs_swave + abs_pwave
+
+        return absorption, quasielastic
 
     def density(self, radius):
-        return self.rho0/(1+np.exp((radius-3.93341372)/0.55863863))
+        return self.rho0/(1+np.exp((radius-3.971)/0.5935))
 
 
 def fit(x, a, b, c):
@@ -117,7 +156,7 @@ if __name__ == '__main__':
     result = quad(test, 0, 225)
     print((3*np.pi**2/2*result[0])**(1/3))
 
-    m_pi = (physics.ParticleInfo(211).mass()+physics.ParticleInfo(111).mass())/2.0
+    m_pi = physics.ParticleInfo(211).mass()
     m_n = physics.ParticleInfo(2212).mass()
     tpi = 165
     epi = tpi+m_pi
@@ -125,12 +164,13 @@ if __name__ == '__main__':
 
     oset = Oset(26, 30)
 
-    radii = np.linspace(0, 7, 50)
+    radii = np.linspace(0, 7, 100)
     rho = oset.density(radii)*(HBARC/m_pi)**3
     scatter = []
     absorbed = []
     for radius in radii:
-        interact = oset(radius, ppi)
+        absorption, interact = oset(radius, ppi)
+        absorbed.append(absorption)
         scatter.append(interact)
 
     oset_data = pd.read_csv('oset.csv',
@@ -140,7 +180,7 @@ if __name__ == '__main__':
 
     # Plot Oset data
     plt.plot(radii, scatter, color='tab:red')
-    # plt.plot(radii, absorbed, color='tab:blue')
+    plt.plot(radii, absorbed, color='tab:blue')
     plt.plot(radii, rho, color='tab:green')
 
     # Plot our calculation
