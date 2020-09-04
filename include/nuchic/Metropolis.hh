decaad14b27b40a71e77190ce9a16a4897d9a6a8
Joshua Isaacson <isaacson@fnal.gov>
2020-09-04 16:21:44 -0500

    Implement basic Metropolis pion interactions
    Begin to get the Metropolis pion and inelastic interactions working. The
    major points to consider are how to handle the absorption and formation
    zones.

--- include/nuchic/Metropolis.hh
+++ include/nuchic/Metropolis.hh
@@ -2,59 +2,176 @@
 #define METROPOLIS_HH
 
 #include <array>
+#include <vector>
 #include <limits>
+
+#include "nuchic/Interactions.hh"
 
 namespace nuchic {
 
 class Particle;
 
-class Metropolis {
-    public:
-        Metropolis() = default;
-        bool IsInelastic(const double &ran, const double &energy, bool isSame) const;
+using Particles = std::vector<Particle>;
 
-    private:
-        static constexpr std::array<double, 8> energies = {335, 410, 510, 660, 840, 1160, 1780, 3900};
-        static constexpr std::array<double, 8> fiiInel = {0.07, 0.20, 0.31, 0.43, 0.58, 0.65, 0.69, 0.69};
-        static constexpr std::array<double, 8> fijInel = {0.04, 0.07, 0.15, 0.27, 0.37, 0.36, 0.35, 0.35};
-        static constexpr std::array<double, 8> fpi = {1.0, 1.0, 1.0, 1.0, 0.97, 0.80, 0.44, 0.44};
-        static constexpr std::array<double, 8> Aii = {0.1, 0.9, 2.7, 9.0, 14.3, 19.2, std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
-        static constexpr std::array<double, 8> Bii = {0, 0, 0, 0, 0, 0, 0, 0};
-        static constexpr std::array<double, 8> Aij = {2.2, 1.8, 2.3, 8.8, 15.0, 29.4, std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
-        static constexpr std::array<double, 8> Bij = {-1.0, -1.1, -0.7, -0.2, 0, 0, 0, 0};
+enum class InteractionType {
+    nucleon, pion
 };
 
 //TODO: come up with better names for these
-enum class PionInteractionMode {
-    same, different, zero
+enum class ChargeMode {
+    ii, ij, zero
 };
 
-class PionInteractions {
+class MetropolisData {
     public:
-        PionInteractions() = default;
-        double xsec(const Particle&, const Particle&) const;
-        bool IsInelastic(const double &ran, const Particle&, const Particle&) const;
+        MetropolisData() = default;
+        MetropolisData(const MetropolisData&) = default;
+        MetropolisData(MetropolisData&&) = default;
+        MetropolisData& operator=(const MetropolisData&) = default;
+        MetropolisData& operator=(MetropolisData&&) = default;
+        virtual ~MetropolisData() = default;
+
+        virtual std::vector<double> XSec(ChargeMode, const Particle&, const Particle&) const = 0;
+        virtual Particles Elastic(ChargeMode, const double&,
+                                  const std::array<double, 2>&,
+                                  const Particle&,
+                                  const Particle&) const = 0;
+        virtual Particles CEX(ChargeMode, const Particle&, const Particle&) const { return {}; }
+        virtual Particles SinglePion(ChargeMode, const std::array<double, 5>&,
+                                     const Particle&, const Particle&) const = 0;
+        virtual Particles DoublePion(ChargeMode, const std::array<double, 5>&,
+                                     const Particle&, const Particle&) const = 0;
+        virtual bool IsAbsorption(ChargeMode, const double&, const double&) const { return false; }
+        virtual bool IsInelastic(ChargeMode, const double&, const double&) const { return false; }
+        virtual bool IsCEX(ChargeMode, const double&, const double&) const { return false; }
+        virtual bool IsSinglePion(ChargeMode, const double&, const double&) const { return false; }
+};
+
+class NucleonData : public MetropolisData {
+    private:
+        static constexpr size_t size = 8;
+        using DataTable = std::array<double, size>;
+
+    public:
+        NucleonData() = default;
+        NucleonData(const NucleonData&) = default;
+        NucleonData(NucleonData&&) = default;
+        NucleonData& operator=(const NucleonData&) = default;
+        NucleonData& operator=(NucleonData&&) = default;
+        ~NucleonData() override = default;
+
+        std::vector<double> XSec(ChargeMode, const Particle&, const Particle&) const override;
+        Particles Elastic(ChargeMode, const double&,
+                          const std::array<double, 2>&,
+                          const Particle&,
+                          const Particle&) const override;
+        Particles SinglePion(ChargeMode, const std::array<double, 5>&,
+                             const Particle&, const Particle&) const override;
+        bool IsInelastic(ChargeMode, const double&, const double&) const override;
+        bool IsSinglePion(ChargeMode, const double&, const double&) const override;
 
     private:
-        PionInteractionMode GetMode(const Particle&, const Particle&) const;
+        int PionCharge(const double&, int) const;
+        std::pair<int, int> DoublePionCharge(const double&, int) const;
 
-        static constexpr std::array<double, 8> energies = {49, 85, 128, 184, 250, 350, 540, 1300};
-        static constexpr std::array<double, 8> sigmaii = {16, 50, 114, 200, 110, 51, 20, 30};
-        static constexpr std::array<double, 8> sigmaij = {15, 21, 43, 66, 44, 23, 22, 30};
-        static constexpr std::array<double, 8> sigmaabs = {20, 32, 45, 36, 18, 0, 0, 0};
-        static constexpr std::array<double, 8> fiiInel = {0, 0, 0, 0.03, 0.06, 0.16, 0.30, 0.88};
-        static constexpr std::array<double, 8> fiiCEX = {0, 0, 0, 0, 0, 0, 0, 0};
-        static constexpr std::array<double, 8> fijInel = {0.45, 0.57, 0.62, 0.64, 0.62, 0.56, 0.58, 0.94};
-        static constexpr std::array<double, 8> fijCEX = {1.0, 1.0, 1.0, 0.95, 0.89, 0.72, 0.51, 0.06};
-        static constexpr std::array<double, 8> f0Inel = {0.42, 0.36, 0.36, 0.37, 0.40, 0.50, 0.59, 0.94};
-        static constexpr std::array<double, 8> f0CEX = {1.0, 1.0, 1.0, 0.90, 0.84, 0.67, 0.50, 0.05};
-        static constexpr std::array<double, 8> fpi = {1.0, 1.0, 1.0, 1.0, 1.0, 0.98, 0.91, 0.24};
-        static constexpr std::array<double, 8> Aii = {3.2, 2.2, 1.9, 2.2, 2.6, 3.0, 3.0, 3.0};
-        static constexpr std::array<double, 8> Bii = {-1.8, -2.1, -1.5, -0.3, 2.0, 4.0, 4.0, 4.0};
-        static constexpr std::array<double, 8> Aij = {1.1, 1.9, 2.2, 2.2, 2.0, 2.7, 3.0, 3.0};
-        static constexpr std::array<double, 8> Bij = {0.8, 0.7, 0.8, 1.0, 1.4, 2.6, 3.6, 4.0};
-        static constexpr std::array<double, 8> A0 = {3.4, 2.1, 1.9, 2.1, 2.5, 3.0, 3.0, 3.0};
-        static constexpr std::array<double, 8> B0 = {-1.8, -2.0, -1.4, 0, 1.7, 4.0, 4.0, 4.0};
+        // Data
+        static constexpr DataTable energies = {335, 410, 510, 660, 840, 1160, 1780, 3900};
+        static constexpr DataTable sigmaii = {24.5, 26.4, 30.4, 41.2, 47.2, 48.0, 44.2, 41.0};
+        static constexpr DataTable sigmaij = {33.0, 34.0, 35.1, 36.5, 37.9, 40.2, 42.7, 42.0};
+        static constexpr DataTable fiiInel = {0.07, 0.20, 0.31, 0.43, 0.58, 0.65, 0.69, 0.69};
+        static constexpr DataTable fijInel = {0.04, 0.07, 0.15, 0.27, 0.37, 0.36, 0.35, 0.35};
+        static constexpr DataTable fpi = {1.0, 1.0, 1.0, 1.0, 0.97, 0.80, 0.44, 0.44};
+        static constexpr DataTable Aii = {0.1, 0.9, 2.7, 9.0, 14.3, 19.2, std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
+        static constexpr DataTable Bii = {0, 0, 0, 0, 0, 0, 0, 0};
+        static constexpr DataTable Aij = {2.2, 1.8, 2.3, 8.8, 15.0, 29.4, std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
+        static constexpr DataTable Bij = {-1.0, -1.1, -0.7, -0.2, 0, 0, 0, 0};
+        static constexpr std::array<double, 2> fpi0 = {0.11, 0.43};
+        static constexpr std::array<double, 4> f2pi0 = {0.6, 0.2, 0.16, 0.10};
+};
+
+class PionData : public MetropolisData {
+    private:
+        static constexpr size_t size = 8;
+        using DataTable = std::array<double, size>;
+
+    public:
+        PionData() = default;
+        PionData(const PionData&) = default;
+        PionData(PionData&&) = default;
+        PionData& operator=(const PionData&) = default;
+        PionData& operator=(PionData&&) = default;
+        ~PionData() override = default;
+
+        std::vector<double> XSec(ChargeMode, const Particle&, const Particle&) const override;
+        Particles Elastic(ChargeMode, const double&,
+                          const std::array<double, 5>&,
+                          const Particle&,
+                          const Particle&) const override;
+        bool IsInelastic(ChargeMode, const double&, const double&) const override;
+        bool IsSinglePion(ChargeMode, const double&, const double&) const override;
+
+
+    private:
+        double XSecii(const Particle&) const;
+        double XSecij(const Particle&) const;
+        double XSecAbs(const Particle&) const;
+
+        // Data
+        static constexpr DataTable energies = {49, 85, 128, 184, 250, 350, 540, 1300};
+        static constexpr DataTable sigmaii = {16, 50, 114, 200, 110, 51, 20, 30};
+        static constexpr DataTable sigmaij = {15, 21, 43, 66, 44, 23, 22, 30};
+        static constexpr DataTable sigmaabs = {20, 32, 45, 36, 18, 0, 0, 0};
+        static constexpr DataTable fiiInel = {0, 0, 0, 0.03, 0.06, 0.16, 0.30, 0.88};
+        static constexpr DataTable fiiCEX = {0, 0, 0, 0, 0, 0, 0, 0};
+        static constexpr DataTable fijInel = {0.45, 0.57, 0.62, 0.64, 0.62, 0.56, 0.58, 0.94};
+        static constexpr DataTable fijCEX = {1.0, 1.0, 1.0, 0.95, 0.89, 0.72, 0.51, 0.06};
+        static constexpr DataTable f0Inel = {0.42, 0.36, 0.36, 0.37, 0.40, 0.50, 0.59, 0.94};
+        static constexpr DataTable f0CEX = {1.0, 1.0, 1.0, 0.90, 0.84, 0.67, 0.50, 0.05};
+        static constexpr DataTable fpi = {1.0, 1.0, 1.0, 1.0, 1.0, 0.98, 0.91, 0.24};
+        static constexpr DataTable Aii = {3.2, 2.2, 1.9, 2.2, 2.6, 3.0, 3.0, 3.0};
+        static constexpr DataTable Bii = {-1.8, -2.1, -1.5, -0.3, 2.0, 4.0, 4.0, 4.0};
+        static constexpr DataTable Aij = {1.1, 1.9, 2.2, 2.2, 2.0, 2.7, 3.0, 3.0};
+        static constexpr DataTable Bij = {0.8, 0.7, 0.8, 1.0, 1.4, 2.6, 3.6, 4.0};
+        static constexpr DataTable A0 = {3.4, 2.1, 1.9, 2.1, 2.5, 3.0, 3.0, 3.0};
+        static constexpr DataTable B0 = {-1.8, -2.0, -1.4, 0, 1.7, 4.0, 4.0, 4.0};
+};
+
+class Metropolis : public Interactions {
+    public:
+        Metropolis(const std::string&);
+
+        static std::unique_ptr<Interactions> Create(const std::string &data) {
+            return std::make_unique<Metropolis>(data);
+        }
+
+        static std::string GetName() { return "Metropolis"; }
+
+        // These functions are defined in the base class
+        static bool IsRegistered() noexcept { return registered; }
+        double CrossSection(const Particle&, const Particle&) const override;
+        std::vector<double> CrossSections(const Particle&, const Particle&) const override;
+        std::vector<Particle> GenerateFinalState(randutils::mt19937_rng&, const Particle&,
+                                                 const Particle&) override;
+    private:
+        // Functions
+        ChargeMode GetMode(const Particle&, const Particle&);
+        std::vector<double> XSec(ChargeMode, const Particle&) const;
+        Particles Absorption(ChargeMode, const Particle&, const Particle&) const;
+        Particles Elastic(ChargeMode, const double&, const Particle&, const Particle&) const;
+        Particles CEX(ChargeMode, const Particle&, const Particle&) const;
+        Particles SinglePion(ChargeMode, const Particle&, const Particle&) const;
+        Particles DoublePion(ChargeMode, const Particle&, const Particle&) const;
+
+        bool IsAbsorption(ChargeMode, const double&, const double&) const;
+        bool IsInelastic(ChargeMode, const double&, const double&) const;
+        bool IsCEX(ChargeMode, const double&, const double&) const;
+        bool IsSinglePion(ChargeMode, const double&, const double&) const;
+
+        // Data
+        static bool registered;
+        std::shared_ptr<NucleonData> nucleon_data;
+        std::shared_ptr<PionData> pion_data;
+        std::shared_ptr<MetropolisData> current_data;
 };
 
 }
--- src/nuchic/Metropolis.cc
+++ src/nuchic/Metropolis.cc
@@ -1,80 +1,330 @@
 #include "nuchic/Metropolis.hh"
 #include "nuchic/Particle.hh"
+#include "nuchic/Utilities.hh"
 
-bool nuchic::Metropolis::IsInelastic(const double &ran, const double &energy, bool isSame) const {
-    if(energy < energies[0]) return false;
+REGISTER_INTERACTION(nuchic::Metropolis);
+using Particles = std::vector<nuchic::Particle>;
 
-    auto idx = static_cast<size_t>(
-            std::distance(energies.begin(),
-                std::lower_bound(energies.begin(), energies.end(), energy)));
-
-    auto t = energy/energies[idx];
-    auto fInelMin = isSame ? fiiInel[idx] : fijInel[idx];
-    auto fInelMax = isSame ? fiiInel[idx+1] : fijInel[idx+1];
-
-    return fInelMin + t*(fInelMax-fInelMin) < ran ? true : false;
+std::vector<double> nuchic::NucleonData::XSec(ChargeMode charge,
+                                              const Particle &part1,
+                                              const Particle &part2) const {
+    auto sigma = (charge == ChargeMode::ii) ? sigmaii : sigmaij;
+    auto Tk = part1.Momentum().Tk();
+    return { Lerp(Tk, energies, sigma) };
 }
 
-double nuchic::PionInteractions::xsec(const nuchic::Particle &pion,
-                                      const nuchic::Particle &nucleon) const {
+bool nuchic::NucleonData::IsInelastic(ChargeMode charge,
+                                      const double &kinetic,
+                                      const double &ran) const {
+    auto fInel = (charge == ChargeMode::ii) ? fiiInel : fijInel;
+    return Lerp(kinetic, energies, fInel) < ran;
+}
+
+bool nuchic::NucleonData::IsSinglePion(ChargeMode,
+                                       const double &kinetic,
+                                       const double &ran) const {
+    return Lerp(kinetic, energies, fpi) < ran; 
+}
+
+Particles nuchic::NucleonData::Elastic(ChargeMode charge,
+                                       const double &kinetic,
+                                       const std::array<double, 2> &rans,
+                                       const Particle &part1,
+                                       const Particle &part2) const {
+
+    // Find the angular coefficient and invert the CDF numerically
+    const auto ATable = (charge == ChargeMode::ii) ? Aii : Aij;
+    const auto BTable = (charge == ChargeMode::ii) ? Bii : Bij;
+    const auto A = Lerp(kinetic, energies, ATable);
+    const auto B = Lerp(kinetic, energies, BTable);
+    std::function<double(double)> func = [&](double x) {
+        const double norm = 5/(2*(A+5));
+        return norm*(A/5*pow(x, 5)+B/4*pow(x, 4)+x+A/5-B/4+1)-rans[0];
+    };
+    auto brent = Brent(func);
+
+    double pcm = part1.Momentum().P();
+    double costheta = brent.CalcRoot(-1, 1);
+    double sintheta = std::sqrt(1-costheta*costheta);
+    double phi = 2*M_PI*rans[1];
+
+    // Setup the outgoing in the CM frame
+    Particles outgoing = { part1, part2 };
+    outgoing[0].SetMomentum({pcm*sintheta*cos(phi),
+                             pcm*sintheta*sin(phi),
+                             pcm*costheta, part1.E()});
+    outgoing[1].SetMomentum({-pcm*sintheta*cos(phi),
+                             -pcm*sintheta*sin(phi),
+                             -pcm*costheta, part1.E()});
+
+    return outgoing;
+}
+
+Particles nuchic::NucleonData::SinglePion(ChargeMode charge, const std::array<double, 5> &rans,
+                                          const Particle &part1,
+                                          const Particle &part2) const {
+    // Evenly split momentum over all three particles
+    double pcm = part1.Momentum().P()*2/3; 
+    double ecm = part1.E()*2/3;
+
+    // Angles for first particle
+    double costheta1 = 2*rans[0] - 1;
+    double sintheta1 = std::sqrt(1-costheta1*costheta1);
+    double phi1 = 2*M_PI*rans[1];
+
+    // Angles for second particle
+    double costheta2 = 2*rans[2] - 1;
+    double sintheta2 = std::sqrt(1-costheta2*costheta2);
+    double phi2 = 2*M_PI*rans[3];
+
+    // Get pids for all three particles
+    int totCharge = static_cast<int>(part1.Info().Charge() + part2.Info().Charge());
+    int pionCharge = PionCharge(rans[4], totCharge);
+}
+
+int nuchic::NucleonData::PionCharge(const double &ran, int totCharge) const {
+    auto frac = fpi0[totCharge % 2]; 
+    if(ran < frac) {
+        return 0;
+    } else {
+        if(totCharge == 1 && ran < frac + (1-frac)/2)
+            return 1;
+        else if(totCharge == 1)
+            return -1;
+        else
+            return totCharge-1;
+    }
+}
+
+std::pair<int, int> nuchic::NucleonData::DoublePionCharge(const double &ran,
+                                                          int totCharge) const {
+    if(ran < f2pi0[0]) {
+        return { 0, 0 };
+    } else if(ran < f2pi0[0] + f2pi0[1]) {
+        return { 1, -1 };
+    } else {
+        ran -= f2pi0[0] + f2pi0[1];
+        if(totCharge == 2) {
+            if(ran < f2pi0[2]) return { 1, 0 };
+            else return { 1, 1 };
+        } else if(totCharge == 0) {
+            if(ran < f2pi0[2]) return { -1, 0 };
+            else return { -1, -1 };
+        } else {
+            if(ran < f2pi0[3]) return { 1, 0 };
+            else return { -1 , 0 };
+        }
+    }
+}
+
+std::vector<double> nuchic::PionData::XSec(int mode, const Particle &pion) const {
+    switch(mode) {
+        case 2:
+            return {XSecii(pion), 0};
+        case 1:
+            return {XSecij(pion), XSecAbs(pion)};
+        case 0:
+            return {0.5*XSecii(pion)+0.5*XSecij(pion),
+                    0.5*XSecAbs(pion)};
+    }
+}
+
+bool nuchic::PionData::IsAbsorption(const double &ran,
+                                    const std::vector<double> &xsec) const {
+    return ran < xsec[0]/xsec[1];
+}
+
+bool nuchic::PionData::IsInelastic(const double &ran,
+                                   const nuchic::Particle &pion,
+                                   const nuchic::Particle &nucleon) const {
     auto mode = GetMode(pion, nucleon);
     auto kinetic = pion.Momentum().Tk();
-
-    if(kinetic < energies[0]) {
-        double gamma = pion.E()/pion.Mass();
-        double eta = pion.Momentum().P()/pion.Mass();
-        switch(mode) {
-            case nuchic::PionInteractionMode::same:
-                return 3.7+286*pow(gamma-1, 3);
-            case nuchic::PionInteractionMode::different:
-                return 6.5+23.9*(gamma-1);
-            case nuchic::PionInteractionMode::zero:
-                return 16.4*(0.14+eta*eta)/eta;
-        }
-    } else if(kinetic > energies.back()) {
-        switch(mode) {
-            case nuchic::PionInteractionMode::same:
-                return sigmaii.back();
-            case nuchic::PionInteractionMode::different:
-                return sigmaij.back();
-            case nuchic::PionInteractionMode::zero:
-                return (sigmaij.back() + sigmaii.back())/2;
-        }
-    }
-
-    auto idx = static_cast<size_t>(
-            std::distance(energies.begin(),
-                std::lower_bound(energies.begin(), energies.end(), kinetic)));
-
-    auto t = kinetic/energies[idx];
-    double sigmaMin{}, sigmaMax{};
-
     switch(mode) {
         case nuchic::PionInteractionMode::same:
-            sigmaMin = sigmaii[idx];
-            sigmaMax = sigmaii[idx+1];
-            break;
+            return ran < Lerp(kinetic, energies, fiiInel);
         case nuchic::PionInteractionMode::different:
-            sigmaMin = sigmaij[idx];
-            sigmaMax = sigmaij[idx+1];
-            break;
+            return ran < Lerp(kinetic, energies, fijInel);
         case nuchic::PionInteractionMode::zero:
-            sigmaMin = (sigmaii[idx] + sigmaij[idx])/2;
-            sigmaMax = (sigmaii[idx+1] + sigmaij[idx+1])/2;
-            break;
+            return ran < Lerp(kinetic, energies, f0Inel);
     }
-
-    return sigmaMin + t*(sigmaMax - sigmaMin);
 }
 
-nuchic::PionInteractionMode nuchic::PionInteractions::GetMode(const nuchic::Particle &pion,
-                                                              const nuchic::Particle &nucleon) const {
-    // Get the interaction mode
-    if((pion.ID() == nuchic::PID::pionp() && nucleon.ID() == nuchic::PID::proton()) 
-        || (pion.ID() == nuchic::PID::pionm() && pion.ID() == nuchic::PID::neutron())) {
-        return PionInteractionMode::same;
-    } else if(pion.ID() == nuchic::PID::pion0()) {
-        return PionInteractionMode::zero;
+bool nuchic::PionData::IsCEX(const double &ran,
+                             const nuchic::Particle &pion,
+                             const nuchic::Particle &nucleon) const {
+    auto mode = GetMode(pion, nucleon);
+    auto kinetic = pion.Momentum().Tk();
+    switch(mode) {
+        case nuchic::PionInteractionMode::same:
+            return ran < Lerp(kinetic, energies, fiiCEX);
+        case nuchic::PionInteractionMode::different:
+            return ran < Lerp(kinetic, energies, fijCEX);
+        case nuchic::PionInteractionMode::zero:
+            return ran < Lerp(kinetic, energies, f0CEX);
     }
-    return PionInteractionMode::different;
+}
+
+bool nuchic::PionData::IsSinglePion(const double &ran,
+                                    const nuchic::Particle &pion,
+                                    const nuchic::Particle&) const {
+    auto kinetic = pion.Momentum().Tk();
+    return ran < Lerp(kinetic, energies, fpi);
+}
+
+double nuchic::PionData::XSecii(const Particle &pion) const {
+    auto kinetic = pion.Momentum().Tk();
+    auto gamma = pion.E()/pion.Mass();
+    if(kinetic < energies[0]) {
+        return 3.7+286*pow(gamma-1, 3);
+    } else if(kinetic > energies.back()) {
+        return sigmaii.back();
+    }
+
+    return Lerp(kinetic, energies, sigmaii);
+}
+
+double nuchic::PionData::XSecij(const Particle &pion) const {
+    auto kinetic = pion.Momentum().Tk();
+    auto gamma = pion.E()/pion.Mass();
+    if(kinetic < energies[0]) {
+        return 6.5+23.9*(gamma-1);
+    } else if(kinetic > energies.back()) {
+        return sigmaij.back();
+    }
+
+    return Lerp(kinetic, energies, sigmaij);
+}
+
+double nuchic::PionData::XSecAbs(const Particle &pion) const {
+    auto kinetic = pion.Momentum().Tk();
+    double eta = pion.Momentum().P()/pion.Mass();
+    if(kinetic < energies[0]) {
+        return 16.4*(0.14+eta*eta)/eta;
+    } else if(kinetic > energies.back()) {
+        return sigmaabs.back();
+    }
+
+    return Lerp(kinetic, energies, sigmaabs);
+}
+
+nuchic::Metropolis::Metropolis(const std::string&) {
+    nucleon_data = std::make_shared<nuchic::NucleonData>(); 
+    pion_data = std::make_shared<nuchic::PionData>(); 
+}
+
+double nuchic::Metropolis::CrossSection(const Particle &part1, const Particle &part2) const {
+    auto xsec = CrossSections(part1, part2);
+
+    return std::accumulate(xsec.begin(), xsec.end(), 0);
+}
+
+std::vector<double> nuchic::Metropolis::CrossSections(const Particle &part1,
+                                                      const Particle &part2) const {
+    if(part1.ID() == PID::neutron() || part1.ID() == PID::proton()) {
+        return nucleon_data -> XSec(part1, part2);
+    } else {
+        return pion_data -> XSec(part1, part2);
+    }
+}
+
+Particles nuchic::Metropolis::GenerateFinalState(randutils::mt19937_rng &rng,
+                                                 const Particle &part1,
+                                                 const Particle &part2) {
+    auto mode = GetMode(part1, part2);
+    auto kinetic = part1.Momentum().Tk();
+
+    // Boost to the CM frame
+    auto part1CM = part1;
+    auto part2CM = part2;
+    auto boostCM = (part1.Momentum() + part2.Momentum()).BoostVector();
+    part1CM.SetMomentum(part1CM.Momentum().Boost(-boostCM));
+    part2CM.SetMomentum(part2CM.Momentum().Boost(-boostCM));
+
+    Particles outgoing{};
+
+    // TODO: Figure out how to handle absorption
+    // if(IsAbsorption(mode, kinetic, rng.uniform(0.0, 1.0))) 
+    //     outgoing =  Absorption(mode, part1CM, part2CM);
+    if(IsInelastic(mode, kinetic, rng.uniform(0.0, 1.0))) {
+        if(IsCEX(mode, kinetic, rng.uniform(0.0, 1.0))) {
+            outgoing = CEX(mode, part1CM, part2CM);
+        } else {
+            if(IsSinglePion(mode, kinetic, rng.uniform(0.0, 1.0))) {
+                outgoing = SinglePion(mode, part1CM, part2CM);
+            }
+            outgoing = DoublePion(mode, part1CM, part2CM);
+        }
+    } else outgoing = Elastic(mode, kinetic, part1CM, part2CM);
+
+    // Return to lab frame
+    for(auto part : outgoing) part.SetMomentum(part.Momentum().Boost(boostCM));
+
+    return outgoing;
+}
+
+nuchic::ChargeMode nuchic::Metropolis::GetMode(const Particle &part1, const Particle &part2) {
+    auto pid1 = part1.ID(), pid2 = part2.ID();
+
+    if(abs(pid1) < 1000) current_data = pion_data;
+    else current_data = nucleon_data;
+
+    if(pid1 == PID::pion0())
+        return nuchic::ChargeMode::zero;
+    else if((pid1 == PID::pionp() && pid2 == PID::proton())
+            || (pid1 == PID::pionm() && pid2 == PID::neutron())
+            || (pid1 == pid2))
+        return nuchic::ChargeMode::ii;
+    else
+        return nuchic::ChargeMode::ij;
+}
+
+std::vector<double> nuchic::Metropolis::XSec(ChargeMode mode, const Particle &part1) const {
+    return current_data -> XSec(mode, part1);
+}
+
+// TODO: Figure out how to handle the absorption
+Particles nuchic::Metropolis::Absorption(ChargeMode mode,
+                                         const Particle &part1,
+                                         const Particle &part2) const {
+}
+
+Particles nuchic::Metropolis::Elastic(ChargeMode mode,
+                                      const double &kinetic,
+                                      const Particle &part1,
+                                      const Particle &part2) const {
+    return current_data -> Elastic(mode, kinetic, part1, part2);
+}
+
+Particles nuchic::Metropolis::CEX(ChargeMode mode,
+                                  const Particle &part1, 
+                                  const Particle &part2) const {
+    return current_data -> CEX(mode, part1, part2);
+}
+
+Particles nuchic::Metropolis::SinglePion(ChargeMode mode,
+                                         const Particle &part1, 
+                                         const Particle &part2) const {
+    return current_data -> SinglePion(mode, part1, part2);
+}
+
+Particles nuchic::Metropolis::DoublePion(ChargeMode mode,
+                                         const Particle &part1, 
+                                         const Particle &part2) const {
+    return current_data -> DoublePion(mode, part1, part2);
+}
+
+bool nuchic::Metropolis::IsAbsorption(ChargeMode mode, const double &kinetic, const double &ran) const {
+    return current_data -> IsAbsorption(mode, kinetic, ran);
+}
+
+bool nuchic::Metropolis::IsInelastic(ChargeMode mode, const double &kinetic, const double &ran) const {
+    return current_data -> IsInelastic(mode, kinetic, ran);
+}
+
+bool nuchic::Metropolis::IsCEX(ChargeMode mode, const double &kinetic, const double &ran) const {
+    return current_data -> IsInelastic(mode, kinetic, ran);
+}
+
+bool nuchic::Metropolis::IsSinglePion(ChargeMode mode, const double &kinetic, const double &ran) const {
+    return current_data -> IsInelastic(mode, kinetic, ran);
 }
