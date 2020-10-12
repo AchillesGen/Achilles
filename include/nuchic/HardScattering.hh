20e49ed2ac08a42e58b18b1a0de7b535e939cfc0
Joshua Isaacson <isaacson@fnal.gov>
2020-10-12 13:19:40 -0500

    Implement test case for phase space generation

--- include/nuchic/HardScattering.hh
+++ include/nuchic/HardScattering.hh
@@ -1,7 +1,18 @@
 #ifndef HARD_SCATTERING_HH
 #define HARD_SCATTERING_HH
 
+#include <utility>
 #include <vector>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
+#include "yaml-cpp/yaml.h"
+#pragma GCC diagnostic pop
+
+#include "nuchic/Beams.hh"
+#include "nuchic/Nucleus.hh"
+
+#include "nuchic/Histogram.hh"
 
 namespace nuchic {
 
@@ -21,40 +32,78 @@ enum class HardScatteringType {
 
 class HardScattering {
     public:
-        HardScattering() = default;
-        HardScattering(const HardScattering&) = default;
-        HardScattering(HardScattering&&) = default;
-        HardScattering& operator=(const HardScattering&) = default;
-        HardScattering& operator=(HardScattering&&) = default;
+        HardScattering(Beam leptonBeam, std::shared_ptr<Nucleus> nuc) 
+            : m_leptonBeam{std::move(leptonBeam)}, m_nuc{std::move(nuc)} {};
+ //       HardScattering(const HardScattering&) = default;
+ //       HardScattering(HardScattering&&) = default;
+ //       HardScattering& operator=(const HardScattering&) = default;
+ //       HardScattering& operator=(HardScattering&&) = default;
         virtual ~HardScattering() = default;
 
         // Validation information
         virtual HardScatteringType ScatteringType() const = 0;
 
         // Calculation details
-        virtual size_t PhaseSpaceSize() const = 0;
-        virtual Particles GeneratePhaseSpace(const std::vector<double>&) const = 0;
         virtual double CrossSection(const Particles&) const = 0;
-        virtual bool HasInitialState() const { return false; }
-        virtual FourVector GenInitialState(const std::vector<FourVector>&) const;
+
+        // Number of phase space variables
+        int NVariables() const { return LeptonVariables() + HadronVariables(); }
+        virtual int LeptonVariables() const = 0;
+        virtual int HadronVariables() const = 0;
+
+        // Generate phase space momentums
+        virtual Particles GeneratePhaseSpace(const std::vector<double>&) const;
+        virtual Particles GenerateLeptons(const std::vector<double>&) const;
+        virtual Particles GenerateHadrons(const std::vector<double>&, const FourVector&) const = 0;
+
+        // Phase space weight
+        virtual double PhaseSpaceWeight(const Particles&) const;
+        virtual double LeptonWeight(const Particles&) const;
+        virtual double HadronWeight(const Particles&) const = 0;
+
+        // Nucleus information
+        const std::shared_ptr<Nucleus>& GetNucleus() const { return m_nuc; }
+        std::shared_ptr<Nucleus>& GetNucleus() { return m_nuc; }
+
+        // Beam information
+        const Beam& GetBeam() const { return m_leptonBeam; }
+
+        // Test Phasespace
+        void SetHist(bool fill) { m_fill = fill; }
+        const Histogram& GetHist() const { return hist; }
+        double Test(const std::vector<double>&, const double&);
+
+    private:
+        bool m_fill{false};
+        Histogram hist{500, 0, 1000, "Test"};
+        Beam m_leptonBeam;
+        std::shared_ptr<Nucleus> m_nuc;
 
 };
 
-class Quasielastic : HardScattering {
+class Quasielastic : public HardScattering {
     public:
+        Quasielastic(Beam beam, std::shared_ptr<Nucleus> nuc) : HardScattering(beam, nuc) {}
         HardScatteringType ScatteringType() const override { 
             return HardScatteringType::Quasielastic;
         }
+
+        int LeptonVariables() const override { return 3 + GetBeam().NVariables(); }
+
+        double CrossSection(const Particles&) const override = 0;
 };
 
-class QESpectral : Quasielastic {
+class QESpectral : public Quasielastic {
     public:
-       size_t PhaseSpaceSize() const override { return 10; }
-       Particles GeneratePhaseSpace(const std::vector<double>&) const override;
-       double CrossSection(const Particles&) const override;
+        QESpectral(Beam beam, std::shared_ptr<Nucleus> nuc) : Quasielastic(beam, nuc) {}
 
-    private:
-       Tensor<2> HadronicTensor(const Particles&) const;
+        int HadronVariables() const override { return 4; }
+        Particles GenerateHadrons(const std::vector<double>&, const FourVector&) const override;
+        double HadronWeight(const Particles&) const override;
+
+        double CrossSection(const Particles&) const override {
+            return 10;
+        }
 };
 
 class DIS : HardScattering {
@@ -62,8 +111,6 @@ class DIS : HardScattering {
         HardScatteringType ScatteringType() const override {
             return HardScatteringType::DeepInelastic;
         }
-
-        size_t PhaseSpaceSize() const override { return 11; }
 };
 
 }
--- src/nuchic/HardScattering.cc
+++ src/nuchic/HardScattering.cc
@@ -1,43 +1,94 @@
 #include "nuchic/HardScattering.hh"
+#include "nuchic/Constants.hh"
 #include "nuchic/Particle.hh"
 
-nuchic::Particles nuchic::QESpectral::GeneratePhaseSpace(const std::vector<double> &rand) const {
-    Particles external = {nuchic::Particle(), nuchic::Particle(),
-                          nuchic::Particle(), nuchic::Particle()};
+// Aliases for most common types
+using nuchic::Particles;
+using nuchic::HardScattering;
+using nuchic::QESpectral;
 
-    // Incoming Electron (No random numbers)
-    FourVector mom0;
-    /* Code to implement the Initial electron momentum */
-    external[0].SetMomentum(mom0);
+Particles HardScattering::GeneratePhaseSpace(const std::vector<double> &rans) const {
+    std::vector<double> leptonRans(rans.begin(), rans.begin() + LeptonVariables());
+    std::vector<double> hadronRans(rans.begin() + LeptonVariables(), rans.end());
+    auto leptons = GenerateLeptons(leptonRans);
 
-    // Incoming Nucleon (3 random numbers)
-    FourVector mom1;
-    /* Code to implement the Spectral Function */
-    external[1].SetMomentum(mom1);
+    FourVector Q = leptons[0].Momentum();
+    for(const auto &lepton : leptons) {
+        if(lepton == leptons[0]) continue;
+        Q -= lepton.Momentum();
+    }
 
-    // Outgoing Electron (3 random numbers)
-    FourVector mom2;
-    /* Use random numbers to setup momentum */
-    external[2].SetMomentum(mom2);
+    auto hadrons = GenerateHadrons(hadronRans, Q);
 
-    // Outgoing Nucleon (3 random numbers)
-    FourVector mom3;
-    /* Use random numbers to setup momentum */
-    external[3].SetMomentum(mom3);
+    Particles particles;
+    particles.reserve(leptons.size() + hadrons.size());
+    particles.insert(particles.end(), leptons.begin(), leptons.end());
+    particles.insert(particles.end(), hadrons.begin(), hadrons.end());
 
-    return external;
+    return particles;
 }
 
-double nuchic::QESpectral::CrossSection(const Particles &particles,
-                                        const std::vector<double> &rand) const {
-    double xsec;
-    /* Calculate cross section using the external particles
-     * 1. Obtain hadronic tensor
-     * 2. Obtain leptonic tensor
-     * 3. Contract together
-     */
+Particles HardScattering::GenerateLeptons(const std::vector<double> &rans) const {
+    Particles leptons = { Particle(PID::electron()), Particle(PID::electron()) };
 
-    // If spectral function
-    // Modify incoming nucleon based on rand[0]
-    return xsec;
+    std::vector<double> beamRans(rans.begin(), rans.begin() + GetBeam().NVariables());
+    std::vector<double> leptonRans(rans.begin()+GetBeam().NVariables(), rans.end());
+    leptons[0].SetMomentum(GetBeam().Flux(leptons[0].ID(), beamRans));
+
+    double cosT = 2*leptonRans[0] - 1;
+    double sinT = sqrt(1-cosT*cosT);
+    double phi = 2*M_PI*leptonRans[1];
+    double Elepton = leptons[0].E()*leptonRans[2];
+    leptons[1].SetMomentum({Elepton*sinT*cos(phi),
+                            Elepton*sinT*sin(phi),
+                            Elepton*cosT, Elepton});
+
+    return leptons;
+}
+
+double HardScattering::PhaseSpaceWeight(const Particles &particles) const {
+    Particles leptons(particles.begin(), particles.begin() + 2);
+    Particles hadrons(particles.begin()+2, particles.end());
+
+    return LeptonWeight(leptons) * HadronWeight(hadrons);
+}
+
+double HardScattering::LeptonWeight(const Particles &leptons) const {
+    return leptons[0].E()*leptons[1].E()*leptons[1].Momentum().P()*4*M_PI;
+}
+
+Particles QESpectral::GenerateHadrons(const std::vector<double> &rans, const FourVector &Q) const {
+    Particles hadrons = { Particle(PID::neutron()), Particle(PID::neutron()) };
+
+    double cosT = 2*rans[0] - 1;
+    double sinT = sqrt(1-cosT*cosT);
+    double phi = 2*M_PI*rans[1];
+    double p = GetNucleus() -> FermiMomentum(0)*rans[2];
+
+    ThreeVector tmp = { p*sinT*cos(phi), p*sinT*sin(phi), p*cosT };
+    tmp += Q.Vec3();
+
+    double Epp = sqrt(pow(hadrons[1].Mass(), 2)+tmp.P2()); 
+
+    double Ep = hadrons[0].Mass()+Q.E()-Epp;
+    Ep = Ep > 0 ? Ep : 0;
+
+    hadrons[0].SetMomentum({p*sinT*cos(phi),
+                            p*sinT*sin(phi),
+                            p*cosT, Ep});
+    hadrons[1].SetMomentum({tmp, Epp});
+
+    return hadrons;
+}
+
+double QESpectral::HadronWeight(const Particles &hadrons) const {
+    return GetNucleus() -> FermiMomentum(0)*hadrons[0].Momentum().P()*hadrons[0].E()*4*M_PI;
+}
+
+double HardScattering::Test(const std::vector<double> &rans, const double &wgt) {
+    auto particles = GeneratePhaseSpace(rans);
+    auto weight = PhaseSpaceWeight(particles);
+    if(m_fill) hist.Fill(particles[2].E(), weight*wgt);
+
+    return weight;
 }
