cbb982adff471e23ac1e5d89470818f9eb46273e
Joshua Isaacson <isaacson@fnal.gov>
2020-12-09 17:05:10 -0600

    Implement Event helper functions

--- include/nuchic/Event.hh
+++ include/nuchic/Event.hh
@@ -1,31 +1,89 @@
 #ifndef EVENT_HH
 #define EVENT_HH
 
+#include <map>
+#include <memory>
+#include <utility>
 #include <vector>
+
+#include "nuchic/HardScatteringEnum.hh"
 
 namespace nuchic {
 
+class PID;
+class FourVector;
 class Particle;
+class Nucleus;
+class Beam;
+
+struct InitialState {
+    std::shared_ptr<Beam> beam;
+    std::shared_ptr<Nucleus> nucleus;
+};
 
 using vParticles = std::vector<Particle>;
 
 class Event {
+    struct PhaseSpaceStruct {
+        std::vector<FourVector> momentum;
+        double weight{};
+    };
+
+    struct MatrixElementStruct {
+        std::vector<PID> inital_state;
+        std::vector<PID> final_state;
+        double weight{};
+    };
+
+    using MatrixElementVec = std::vector<MatrixElementStruct>;
+
     public:
         Event() = default;
-        Event(vParticles&& particles) : m_particles{std::move(particles)} {}
-        Event(vParticles&& particles, double wgt) : m_particles{std::move(particles)},
-            m_wgt{std::move(wgt)} {}
+        Event(std::shared_ptr<Nucleus>, std::shared_ptr<Beam>,
+              const std::vector<double>&, double);
 
-        void SetWgt(double wgt) { m_wgt = std::move(wgt); }
-        void SetParticles(vParticles &&particles) { m_particles = std::move(particles); }
+        void SetHardScatteringType(HardScatteringType type) { m_type = type; }
+        void InitializeLeptons(size_t);
+        void InitializeHadrons(const std::vector<std::array<size_t, 3>>&);
+
+        const PhaseSpaceStruct &PhaseSpace() const { return m_ps; }
+        PhaseSpaceStruct &PhaseSpace() { return m_ps; }
+
+        const MatrixElementStruct &MatrixElement(size_t i) const { return m_me[i]; }
+        MatrixElementStruct &MatrixElement(size_t i) { return m_me[i]; }
+
+        const MatrixElementVec &MatrixElements() const { return m_me; }
+        MatrixElementVec &MatrixElements() { return m_me; }
+
+        bool TotalCrossSection();
+        std::vector<double> EventProbs() const;
+        static bool MatrixCompare(const MatrixElementStruct&, double);
+
+        const std::shared_ptr<Nucleus>& CurrentNucleus() const { return m_nuc; }
+        std::shared_ptr<Nucleus>& CurrentNucleus() { return m_nuc; }
+        const std::shared_ptr<Beam>& CurrentBeam() const { return m_beam; }
+        std::shared_ptr<Beam>& CurrentBeam() { return m_beam; }
+
         void AddParticle(const Particle&);
-
-        const vParticles& Particles() const { return m_particles; }
-        const double& Weight() const { return m_wgt; }
+        vParticles Particles() const;
+        const vParticles& Hadrons() const;
+        vParticles& Hadrons();
+        const vParticles& Leptons() const { return m_leptons; }
+        vParticles& Leptons() { return m_leptons; }
+        double Weight() const;
 
     private:
-        vParticles m_particles{};
-        double m_wgt{};
+        static double AddEvents(double, const MatrixElementStruct&);
+
+        bool ValidateEvent(size_t) const;
+
+        HardScatteringType m_type{HardScatteringType::None};
+        std::shared_ptr<Nucleus> m_nuc;
+        std::shared_ptr<Beam> m_beam;
+        PhaseSpaceStruct m_ps{};
+        MatrixElementVec m_me;
+        double m_vWgt{}, m_meWgt{};
+        vParticles m_leptons{};
 };
 
 }
---
+++ src/nuchic/Event.cc
@@ -0,0 +1,106 @@
+#include "nuchic/Event.hh"
+#include "nuchic/Nucleus.hh"
+#include "nuchic/Particle.hh"
+#include "nuchic/Beams.hh"
+
+using nuchic::Event;
+
+Event::Event(std::shared_ptr<Nucleus> nuc, std::shared_ptr<Beam> beam,
+             const std::vector<double> &rans, double vwgt) 
+        : m_nuc{std::move(nuc)}, m_beam{std::move(beam)}, m_vWgt{std::move(vwgt)} {
+    m_nuc -> GenerateConfig();
+    m_ps.momentum.push_back(m_beam -> Flux(PID::electron(), rans));
+}
+
+bool Event::ValidateEvent(size_t imatrix) const {
+    return m_ps.momentum.size() == m_me[imatrix].inital_state.size() + m_me[imatrix].final_state.size();
+}
+
+void Event::InitializeLeptons(size_t imatrix) {
+    if(!ValidateEvent(imatrix))
+        throw std::runtime_error("Phase space and Matrix element have different number of particles");
+
+    size_t idx = 0;
+    for(auto pid : m_me[imatrix].inital_state) {
+        ParticleInfo info(pid);
+        if(info.IsLepton()) {
+            m_leptons.emplace_back(info, m_ps.momentum[idx]);
+            m_leptons.back().SetStatus(ParticleStatus::initial_state);
+        }
+        ++idx;
+    }
+    for(auto pid : m_me[imatrix].final_state) {
+        ParticleInfo info(pid);
+        if(info.IsLepton()) {
+            m_leptons.emplace_back(info, m_ps.momentum[idx]);
+            m_leptons.back().SetStatus(ParticleStatus::final_state);
+        }
+        ++idx;
+    }
+}
+
+void Event::InitializeHadrons(const std::vector<std::array<size_t, 3>> &idxs) {
+    for(const auto &idx : idxs) {
+        m_nuc -> Nucleons()[idx[0]].SetStatus(ParticleStatus::initial_state);
+        m_nuc -> Nucleons()[idx[0]].SetMomentum(m_ps.momentum[idx[1]]);
+        Particle outNucleon(m_nuc -> Nucleons()[idx[0]]);
+        outNucleon.SetStatus(ParticleStatus::propagating);
+        outNucleon.SetMomentum(m_ps.momentum[idx[2]]);
+        m_nuc -> Nucleons().push_back(outNucleon);
+    }
+}
+
+void Event::AddParticle(const Particle &part) {
+    if(part.Info().IsHadron()) {
+        m_nuc -> Nucleons().push_back(part);
+    } else {
+        m_leptons.push_back(part);
+    }
+}
+
+nuchic::vParticles Event::Particles() const {
+    vParticles result;
+    result.insert(result.end(), Hadrons().begin(), Hadrons().end());
+    result.insert(result.end(), m_leptons.begin(), m_leptons.end());
+    return result;
+}
+
+const nuchic::vParticles& Event::Hadrons() const {
+    return m_nuc -> Nucleons();
+}
+
+nuchic::vParticles& Event::Hadrons() {
+    return m_nuc -> Nucleons();
+}
+
+double Event::Weight() const {
+    static constexpr double conv = 1e6;
+
+    if(!ValidateEvent(0))
+        throw std::runtime_error("Phase space and Matrix element have different number of particles");
+    return m_ps.weight*m_vWgt*m_meWgt*conv;
+}
+
+bool Event::TotalCrossSection() {
+    m_meWgt = std::accumulate(m_me.begin(), m_me.end(), 0.0, AddEvents);
+    return m_meWgt > 0 ? true : false;
+}
+
+std::vector<double> Event::EventProbs() const {
+    std::vector<double> probs;
+    probs.push_back(0.0);
+    double cumulative = 0.0;
+    for(const auto & m : m_me) {
+        cumulative += m.weight;
+        probs.emplace_back(cumulative / m_meWgt);
+    }
+    return probs;
+}
+
+bool Event::MatrixCompare(const MatrixElementStruct &m, double value) {
+    return m.weight < value;
+}
+
+double Event::AddEvents(double value, const MatrixElementStruct &m) {
+    return value + m.weight;
+}
