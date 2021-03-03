b1df947268dd8209d343b1ceb7fd88722704917c
Joshua Isaacson <isaacson@fnal.gov>
2021-03-03 15:19:26 -0600

    Move tokenizer to Utilities

--- include/nuchic/Utilities.hh
+++ include/nuchic/Utilities.hh
@@ -141,14 +141,24 @@ constexpr T factorial(const T &x) {
     return x == 0 ? 1 : x * factorial(x-1);
 }
 
-inline std::vector<std::string> tokenize(const std::string &str, char delimiter=' ') {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(str);
-    while(std::getline(tokenStream, token, delimiter)) {
-        tokens.push_back(token);
+template<class ContainerT>
+void tokenize(const std::string& str, ContainerT& tokens,
+              const std::string& delimiters=" \n\t", bool trimEmpty=true) {
+    std::string::size_type lastPos = 0, length = str.length();
+    
+    using value_type = typename ContainerT::value_type;
+    using size_type = typename ContainerT::size_type;
+
+    while(lastPos < length+1) {
+        std::string::size_type pos = str.find_first_of(delimiters, lastPos);
+        if(pos == std::string::npos) pos = length;
+
+        if(pos != lastPos || !trimEmpty) {
+            tokens.push_back(value_type(str.data()+lastPos,
+                             static_cast<size_type>(pos)-lastPos));
+        }
+        lastPos = pos+1;
     }
-    return tokens;
 }
 
 template<size_t N>
--- src/nuchic/Configuration.cc
+++ src/nuchic/Configuration.cc
@@ -4,31 +4,12 @@
 #include "nuchic/Particle.hh"
 #include "nuchic/ThreeVector.hh"
 #include "nuchic/Random.hh"
+#include "nuchic/Utilities.hh"
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow"
 #include "gzstream/gzstream.h"
 #pragma GCC diagnostic pop
-
-template<class ContainerT>
-void tokenize(const std::string& str, ContainerT& tokens,
-              const std::string& delimiters=" \n\t", bool trimEmpty=true) {
-    std::string::size_type lastPos = 0, length = str.length();
-    
-    using value_type = typename ContainerT::value_type;
-    using size_type = typename ContainerT::size_type;
-
-    while(lastPos < length+1) {
-        std::string::size_type pos = str.find_first_of(delimiters, lastPos);
-        if(pos == std::string::npos) pos = length;
-
-        if(pos != lastPos || !trimEmpty) {
-            tokens.push_back(value_type(str.data()+lastPos,
-                             static_cast<size_type>(pos)-lastPos));
-        }
-        lastPos = pos+1;
-    }
-}
 
 nuchic::DensityConfiguration::DensityConfiguration(const std::string &filename) {
     // Load configuration
