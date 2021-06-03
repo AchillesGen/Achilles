ba5c32b46c2a86d2061ef3e838c1855b6ae70b9a
Joshua Isaacson <isaacson@fnal.gov>
2021-06-03 12:15:01 -0500

    Improve Momentum solver with Potentials

--- include/nuchic/MomSolver.hh
+++ include/nuchic/MomSolver.hh
@@ -1,12 +1,16 @@
 #ifndef MOM_SOLVER_HH
 #define MOM_SOLVER_HH
 
+#include <utility>
+
 namespace nuchic {
     class FourVector;
 
     FourVector SolveDelta(const FourVector&, const FourVector&, double, double, double, double);
-    FourVector SolveDeltaWithPotential(const FourVector&, const FourVector&,
-                                       double, double, double, double, double);
+    std::pair<double, double> FindMomentumRange(double, double, double, double, double);
+    FourVector SolveDeltaWithPotential(const FourVector&,
+                                       double, double, double, double, double, double);
+    double Potential(double p, double rho);
 }
 
 #endif
--- src/nuchic/MomSolver.cc
+++ src/nuchic/MomSolver.cc
@@ -25,7 +25,7 @@ nuchic::FourVector nuchic::SolveDelta(const nuchic::FourVector &p1, const nuchic
     return FourVector(p3Mag*sinTheta*cos(phi), p3Mag*sinTheta*sin(phi), p3Mag*cosTheta, energy).Boost(beta);
 }
 
-double Potential(double p, double rho) {
+double nuchic::Potential(double p, double rho) {
     constexpr double rho0 = 0.16;
     rho = rho/rho0;
     const double alpha = 15.52 * rho + 24.93*rho*rho;
@@ -35,53 +35,66 @@ double Potential(double p, double rho) {
     return alpha + beta/(1+pow(k/lambda, 2));
 }
 
-nuchic::FourVector nuchic::SolveDeltaWithPotential(const nuchic::FourVector &p1, const nuchic::FourVector &p2,
-                                                   double m3, double m4,
-                                                   double cosTheta, double phi, double /*rho*/) {
-    const double s = (p1 + p2).M2(); 
-    // const auto beta = (p1 + p2).BoostVector();
-    // const auto pcm = (p1 + p2).Boost(-beta);
-    const double sinTheta = sqrt(1 - pow(cosTheta, 2));
-    auto _func = [&](double p3Mag) {
-        double pot3 = 0; // Potential(p3Mag, rho);
-        double energy = sqrt(p3Mag*p3Mag + m3*m3);
-        FourVector p3(p3Mag*sinTheta*cos(phi),
-                p3Mag*sinTheta*sin(phi), p3Mag*cosTheta, energy + pot3);
-        // FourVector p4 = p1 + p2 - p3;
-        // auto p3cm = p3.Boost(-beta);
-        // double p4Mag = p4.P();
-        // double pot4 = Potential(p4Mag, rho);
-        // double m4U2 = m4*m4+pow(pot4, 2)+2*pot4*sqrt(p4Mag*p4Mag+m4*m4);
-        // return p1.E() + p2.E() - p3.E() - sqrt((p1 + p2 - p3).P2() + m4*m4);
-        return s - 2*(p1+p2)*p3 + m3*m3 - m4*m4;
+std::pair<double, double> nuchic::FindMomentumRange(double E, double q, double m,
+                                                    double rho1, double rho2) {
+    auto _func = [&](double p3, double sign) {
+        return E - sqrt(m*m+p3*p3) - sqrt(m*m+q*q-2*sign*q*p3+p3*p3)
+            - Potential(p3, rho1) - Potential(sqrt(q*q-2*sign*q*p3+p3*p3), rho2);
     };
 
-    FourVector dummy(sinTheta*cos(phi), sinTheta*sin(phi), cosTheta, 1);
-    std::vector<double> guess;
-    double a = (p1.E() + p2.E())/(2*m3);
-    double b = -(p1.P()*p1.CosAngle(dummy)+p2.P()*p2.CosAngle(dummy));
-    double c = m3*(p1.E() + p2.E())-s/2;
-    double det = b*b-4*a*c;
-    double rel_guess = s/(2*(p1.E()+p2.E()-(p1.P()*p1.CosAngle(dummy)+p2.P()*p2.CosAngle(dummy))));
-    guess.emplace_back(det > 0 ? (-b + sqrt(det))/(2*a) : 0);
-    guess.emplace_back(rel_guess);
-    spdlog::info("Guess = {}", fmt::join(guess, ", ")); 
-    spdlog::info("Func(guess) = {}, {}", _func(guess[0]), _func(guess[1]));
-    double x = guess[1];
-    double result_old = -std::numeric_limits<double>::infinity();
-    while(x > -guess[1] && _func(guess[0])*_func(guess[1]) > 0) {
-        double result = _func(x);
-        spdlog::info("Func({}) = {}", x, _func(x));
-        x -= 10;
-        if(result * _func(guess[1]) < 0) break;
-        if(result < result_old) break;
-        result_old = result;
+    auto _funcMin = [&](double p3) {
+        return _func(p3, -1);
+    };
+
+    auto _funcMid = [&](double p3) {
+        return _func(p3, 0);
+    };
+
+    auto _funcMax = [&](double p3) {
+        return _func(p3, 1);
+    };
+
+    spdlog::info("Min: {}, {}", _funcMin(0), _funcMin(2*q));
+    spdlog::info("Mid: {}, {}", _funcMid(0), _funcMid(2*q));
+    spdlog::info("Max: {}, {}", _funcMax(0), _funcMax(2*q));
+    for(size_t i = 0; i < 100; ++i) {
+        double tmp = static_cast<double>(i)*2*q/100;
+        spdlog::info("_funcMin({}) = {}", tmp, _funcMin(tmp));
     }
+    for(size_t i = 0; i < 100; ++i) {
+        double tmp = static_cast<double>(i)*2*q/100;
+        spdlog::info("_funcMid({}) = {}", tmp, _funcMid(tmp));
+    }
+    for(size_t i = 0; i < 100; ++i) {
+        double tmp = static_cast<double>(i)*2*q/100;
+        spdlog::info("_funcMax({}) = {}", tmp, _funcMax(tmp));
+    }
+    Brent brentMin(_funcMin);
+    double pmin = brentMin.CalcRoot(0, 2*q);
+    spdlog::info("Min sol: {}", pmin);
+    Brent brentMax(_funcMax);
+    double pmax = brentMax.CalcRoot(0, 2*q);
+    
+    return {pmin, pmax};
+}
+
+nuchic::FourVector nuchic::SolveDeltaWithPotential(const nuchic::FourVector &q,
+                                                   double m3, double m4,
+                                                   double p3Mag, double phi,
+                                                   double rho3, double rho4) {
+    const double energy = sqrt(p3Mag*p3Mag + m3*m3)+Potential(p3Mag, rho3);
+    auto _func = [&](double cosTheta) {
+        double sinTheta = sqrt(1-cosTheta*cosTheta);
+        FourVector p3 = {p3Mag*sinTheta*cos(phi), p3Mag*sinTheta*sin(phi), p3Mag*cosTheta, energy};
+        auto p4 = q - p3;
+        p4.E() = sqrt(p4.P2() + m4*m4) + Potential(p4.P(), rho4);
+        spdlog::info("p3 = {}, delta = {}", p3, q.E() - p3.E() - p4.E());
+        return q.E() - p3.E() - p4.E();
+    };
 
     Brent brent(_func);
-    double p3Mag = brent.CalcRoot(guess[0], guess[1]);
-    spdlog::info("p3mag = {}", p3Mag);
-    double pot3 = 0; // Potential(p3Mag, rho);
-    double energy = sqrt(p3Mag*p3Mag + m3*m3) + pot3;
+    double cosTheta = brent.CalcRoot(-1, 1);
+    double sinTheta = sqrt(1-cosTheta*cosTheta);
+    spdlog::info("cosTheta = {}", cosTheta);
     return {p3Mag*sinTheta*cos(phi), p3Mag*sinTheta*sin(phi), p3Mag*cosTheta, energy};
 }
