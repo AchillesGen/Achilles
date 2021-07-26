c2a02f2553106cb5f45369a4221ddfc371148c64
Joshua Isaacson <isaacson@fnal.gov>
2021-07-26 10:08:45 -0500

    Update momentum solver
    Now includes the potential class to solve it for arbitrary potentials.

--- include/nuchic/MomSolver.hh
+++ include/nuchic/MomSolver.hh
@@ -1,16 +1,17 @@
 #ifndef MOM_SOLVER_HH
 #define MOM_SOLVER_HH
 
+#include <memory>
 #include <utility>
 
 namespace nuchic {
     class FourVector;
+    class Potential;
 
     FourVector SolveDelta(const FourVector&, const FourVector&, double, double, double, double);
-    std::pair<double, double> FindMomentumRange(double, double, double, double, double);
-    FourVector SolveDeltaWithPotential(const FourVector&,
+    std::pair<double, double> FindMomentumRange(const FourVector&, double rangeExtend = 1.05);
+    FourVector SolveDeltaWithPotential(const FourVector&, const Potential&,
                                        double, double, double, double, double, double);
-    double Potential(double p, double rho);
 }
 
 #endif
--- src/nuchic/MomSolver.cc
+++ src/nuchic/MomSolver.cc
@@ -3,6 +3,7 @@
 #include "nuchic/FourVector.hh"
 #include "nuchic/ThreeVector.hh"
 #include "nuchic/Utilities.hh"
+#include "nuchic/Potential.hh"
 
 #include "spdlog/spdlog.h"
 
@@ -25,69 +26,30 @@ nuchic::FourVector nuchic::SolveDelta(const nuchic::FourVector &p1, const nuchic
     return FourVector(p3Mag*sinTheta*cos(phi), p3Mag*sinTheta*sin(phi), p3Mag*cosTheta, energy).Boost(beta);
 }
 
-double nuchic::Potential(double p, double rho) {
-    constexpr double rho0 = 0.16;
-    rho = rho/rho0;
-    const double alpha = 15.52 * rho + 24.93*rho*rho;
-    const double beta = -116*rho;
-    const double lambda = 3.29 - 0.373*rho;
-    const double k = p/nuchic::Constant::HBARC;
-    return alpha + beta/(1+pow(k/lambda, 2));
-}
+std::pair<double, double> nuchic::FindMomentumRange(const nuchic::FourVector &q_free, double rangeExtend) {
+    std::pair<double, double> range;
+    const double detM = sqrt((q_free.M2()-4*pow(nuchic::Constant::mN, 2))*(q_free.M2() + q_free.P2())/q_free.M2());
+    range.first = q_free.M2() > 2*nuchic::Constant::mN*q_free.E() ? (detM-q_free.P())/2 : (q_free.P()-detM)/2;
+    range.second = (q_free.P()+detM)/2;
+    range.first /= rangeExtend;
+    range.second *= rangeExtend;
 
-std::pair<double, double> nuchic::FindMomentumRange(double E, double q, double m,
-                                                    double rho1, double rho2) {
-    auto _func = [&](double p3, double sign) {
-        return E - sqrt(m*m+p3*p3) - sqrt(m*m+q*q-2*sign*q*p3+p3*p3)
-            - Potential(p3, rho1) - Potential(sqrt(q*q-2*sign*q*p3+p3*p3), rho2);
-    };
-
-    auto _funcMin = [&](double p3) {
-        return _func(p3, -1);
-    };
-
-    auto _funcMid = [&](double p3) {
-        return _func(p3, 0);
-    };
-
-    auto _funcMax = [&](double p3) {
-        return _func(p3, 1);
-    };
-
-    spdlog::info("Min: {}, {}", _funcMin(0), _funcMin(2*q));
-    spdlog::info("Mid: {}, {}", _funcMid(0), _funcMid(2*q));
-    spdlog::info("Max: {}, {}", _funcMax(0), _funcMax(2*q));
-    for(size_t i = 0; i < 100; ++i) {
-        double tmp = static_cast<double>(i)*2*q/100;
-        spdlog::info("_funcMin({}) = {}", tmp, _funcMin(tmp));
-    }
-    for(size_t i = 0; i < 100; ++i) {
-        double tmp = static_cast<double>(i)*2*q/100;
-        spdlog::info("_funcMid({}) = {}", tmp, _funcMid(tmp));
-    }
-    for(size_t i = 0; i < 100; ++i) {
-        double tmp = static_cast<double>(i)*2*q/100;
-        spdlog::info("_funcMax({}) = {}", tmp, _funcMax(tmp));
-    }
-    Brent brentMin(_funcMin);
-    double pmin = brentMin.CalcRoot(0, 2*q);
-    spdlog::info("Min sol: {}", pmin);
-    Brent brentMax(_funcMax);
-    double pmax = brentMax.CalcRoot(0, 2*q);
-    
-    return {pmin, pmax};
+    return range;
 }
 
 nuchic::FourVector nuchic::SolveDeltaWithPotential(const nuchic::FourVector &q,
+                                                   const Potential& potential,
                                                    double m3, double m4,
                                                    double p3Mag, double phi,
-                                                   double rho3, double rho4) {
-    const double energy = sqrt(p3Mag*p3Mag + m3*m3)+Potential(p3Mag, rho3);
+                                                   double radius3, double radius4) {
+    auto potential3 = potential(p3Mag, radius3);
+    const double energy = sqrt(p3Mag*p3Mag + pow(m3 + static_cast<double>(potential3.rscalar), 2)) + static_cast<double>(potential3.rvector);
     auto _func = [&](double cosTheta) {
         double sinTheta = sqrt(1-cosTheta*cosTheta);
         FourVector p3 = {p3Mag*sinTheta*cos(phi), p3Mag*sinTheta*sin(phi), p3Mag*cosTheta, energy};
         auto p4 = q - p3;
-        p4.E() = sqrt(p4.P2() + m4*m4) + Potential(p4.P(), rho4);
+        auto potential4 = potential(p4.P(), radius4);
+        p4.E() = sqrt(p4.P2() + pow(m4 + static_cast<double>(potential4.rscalar), 2)) + static_cast<double>(potential4.rvector);
         return q.E() - p3.E() - p4.E();
     };
 
