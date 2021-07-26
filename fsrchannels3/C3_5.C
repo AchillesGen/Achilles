b6995c9fd1c3764ea77383733822e5a981243036
Diego Lopez <dlopezgu@fnal.gov>
2021-07-26 14:41:35 -0500

    Added multichannel files from Sherpa and simplified them to work with Nuchic

---
+++ fsrchannels3/C3_5.C
@@ -0,0 +1,40 @@
+// Channel_Generator_UniV
+#include "PHASIC++/Channels/Channel_Elements.H"
+
+using namespace PHASIC;
+using namespace ATOOLS;
+
+namespace PHASIC {
+  class C3_5 : public Single_Channel {
+    constexpr static double m_salpha=0.9,m_thexp=0.9;
+    constexpr static double m_amct=1.,m_alpha=0.9,m_ctmax=1.,m_ctmin=-1.;
+  public:
+    void   GenerateWeight(Vec4D *,Cut_Data *, std::vector<double> &);
+    void   GeneratePoint(Vec4D *,Cut_Data *,const std::vector<double> &);
+  };
+}
+
+void C3_5::GeneratePoint(Vec4D * p,Cut_Data * cuts,const std::vector<double> & ran)
+{
+  double s34_max = sqr((p[0]+p[1]).Mass()-sqrt(s2));
+  double s34_min = cuts->GetscutAmegic(std::string("34"));
+  Vec4D  p34;
+  double s34 = CE.MasslessPropMomenta(m_salpha,s34_min,s34_max,ran[0]);
+  double tmass201 = Flavour((kf_code)(23)).Mass();
+  CE.TChannelMomenta(p[0],p[1],p[2],p34,s2,s34,tmass201,m_alpha,m_ctmax,m_ctmin,m_amct,0,ran[1],ran[2]);
+  CE.TChannelMomenta(p[0]-p[2],p[1],p[3],p[4],s3,s4,0.,m_alpha,1.,-1.,m_amct,0,ran[3],ran[4]);
+}
+
+void C3_5::GenerateWeight(Vec4D* p,Cut_Data * cuts, std::vector<double> & rans)
+{
+  double wt = 1.;
+  double s34_max = sqr((p[0]+p[1]).Mass()-sqrt(s2));
+  double s34_min = cuts->GetscutAmegic(std::string("34"));
+  wt *= CE.MasslessPropWeight(m_salpha,s34_min,s34_max,dabs((p[3]+p[4]).Abs2()),rans[0]);
+  double tmass201 = Flavour((kf_code)(23)).Mass();
+  wt *= CE.TChannelWeight(p[0],p[1],p[2],p[3]+p[4],tmass201,m_alpha,m_ctmax,m_ctmin,m_amct,0,rans[1],rans[2]);
+  wt *= CE.TChannelWeight(p[0]-p[2],p[1],p[3],p[4],0.,m_alpha,1.,-1.,m_amct,0,rans[3],rans[4]);
+  if (wt!=0.) wt = 1.0/wt/pow(2.*M_PI,3*3.-4.);
+
+  weight = wt;
+}
---
+++ fsrchannels3/C3_6.C
@@ -0,0 +1,38 @@
+// Channel_Generator_UniV
+#include "PHASIC++/Channels/Channel_Elements.H"
+
+using namespace PHASIC;
+using namespace ATOOLS;
+
+namespace PHASIC {
+  class C3_6 : public Single_Channel {
+    constexpr static double m_salpha=0.9,m_thexp=0.9;
+    constexpr static double m_amct=1.,m_alpha=0.9,m_ctmax=1.,m_ctmin=-1.;
+  public:
+    void   GenerateWeight(Vec4D *,Cut_Data *, std::vector<double> &);
+    void   GeneratePoint(Vec4D *,Cut_Data *,const std::vector<double> &);
+  };
+}
+
+void C3_6::GeneratePoint(Vec4D * p,Cut_Data * cuts,const std::vector<double> & ran)
+{
+  double s34_max = sqr((p[0]+p[1]).Mass()-sqrt(s2));
+  double s34_min = cuts->GetscutAmegic(std::string("34"));
+  Vec4D  p34;
+  double s34 = CE.MasslessPropMomenta(m_salpha,s34_min,s34_max,ran[0]);
+  CE.TChannelMomenta(p[0],p[1],p34,p[2],s34,s2,0.,m_alpha,m_ctmax,m_ctmin,m_amct,0,ran[1],ran[2]);
+  CE.Isotropic2Momenta(p34,s3,s4,p[3],p[4],ran[3],ran[4]);
+}
+
+void C3_6::GenerateWeight(Vec4D* p,Cut_Data * cuts, std::vector<double> & rans)
+{
+  double wt = 1.;
+  double s34_max = sqr((p[0]+p[1]).Mass()-sqrt(s2));
+  double s34_min = cuts->GetscutAmegic(std::string("34"));
+  wt *= CE.MasslessPropWeight(m_salpha,s34_min,s34_max,dabs((p[3]+p[4]).Abs2()),rans[0]);
+  wt *= CE.TChannelWeight(p[0],p[1],p[3]+p[4],p[2],0.,m_alpha,m_ctmax,m_ctmin,m_amct,0,rans[1],rans[2]);
+  wt *= CE.Isotropic2Weight(p[3],p[4],rans[3],rans[4]);
+  if (wt!=0.) wt = 1.0/wt/pow(2.*M_PI,3*3.-4.);
+
+  weight = wt;
+}
