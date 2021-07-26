231a31522880701de25d63778146385507d5f3e0
Diego Lopez <dlopezgu@fnal.gov>
2021-07-26 16:15:11 -0500

    Exchanged Single_Channel for Mapper class in channel files. Created separate header and C++ files for Channels3. Added Channels3.cc to CMakeLists

--- fsrchannels3/C3_5.C
+++ fsrchannels3/C3_5.C
@@ -1,20 +1,21 @@
 // Channel_Generator_UniV
 #include "PHASIC++/Channels/Channel_Elements.H"
+#include "nuchic/Mapper.hh"
 
 using namespace PHASIC;
 using namespace ATOOLS;
 
 namespace PHASIC {
-  class C3_5 : public Single_Channel {
+  class C3_5 : public nuchic::Mapper<Vec4D> {
     constexpr static double m_salpha=0.9,m_thexp=0.9;
     constexpr static double m_amct=1.,m_alpha=0.9,m_ctmax=1.,m_ctmin=-1.;
   public:
-    void   GenerateWeight(Vec4D *,Cut_Data *, std::vector<double> &);
-    void   GeneratePoint(Vec4D *,Cut_Data *,const std::vector<double> &);
+    double   GenerateWeight(std::vector<Vec4D> *, std::vector<double> &) const override;
+    void   GeneratePoint(std::vector<Vec4D> *,const std::vector<double> &) const override;
   };
 }
 
-void C3_5::GeneratePoint(Vec4D * p,Cut_Data * cuts,const std::vector<double> & ran)
+void C3_5::GeneratePoint(std::vector<Vec4D> * p,const std::vector<double> & ran) const
 {
   double s34_max = sqr((p[0]+p[1]).Mass()-sqrt(s2));
   double s34_min = cuts->GetscutAmegic(std::string("34"));
@@ -25,7 +26,7 @@ void C3_5::GeneratePoint(Vec4D * p,Cut_Data * cuts,const std::vector<double> & r
   CE.TChannelMomenta(p[0]-p[2],p[1],p[3],p[4],s3,s4,0.,m_alpha,1.,-1.,m_amct,0,ran[3],ran[4]);
 }
 
-void C3_5::GenerateWeight(Vec4D* p,Cut_Data * cuts, std::vector<double> & rans)
+double C3_5::GenerateWeight(std::vector<Vec4D> * p, std::vector<double> & rans) const
 {
   double wt = 1.;
   double s34_max = sqr((p[0]+p[1]).Mass()-sqrt(s2));
@@ -37,4 +38,5 @@ void C3_5::GenerateWeight(Vec4D* p,Cut_Data * cuts, std::vector<double> & rans)
   if (wt!=0.) wt = 1.0/wt/pow(2.*M_PI,3*3.-4.);
 
   weight = wt;
+  return weight
 }
