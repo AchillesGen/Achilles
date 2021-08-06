013abc8426fcb14d49cfb6a1cb35a8bb84a24157
Joshua Isaacson <isaacson@fnal.gov>
2021-08-06 14:28:15 -0500

    Implement QuasielasticTestMapper
    This mapper is based off the old way of handling the phase space. It
    will only work for 2 -> 2 scattering, but it is useful to have for
    debugging purposes.

---
+++ include/nuchic/QuasielasticTestMapper.hh
@@ -0,0 +1,38 @@
+#ifndef QUASIELASTIC_TEST_MAPPER_HH
+#define QUASIELASTIC_TEST_MAPPER_HH
+
+#include <cmath>
+#include <iostream>
+#include "nuchic/Mapper.hh"
+#include "nuchic/RunModes.hh"
+
+namespace YAML {
+    class Node;
+}
+
+namespace nuchic {
+
+class FourVector;
+class Beam;
+
+class QuasielasticTestMapper : public Mapper<FourVector> {
+    public:
+        QuasielasticTestMapper(const YAML::Node&, std::shared_ptr<Beam>);
+
+        void GeneratePoint(std::vector<FourVector>&, const std::vector<double>&) const override;
+        double GenerateWeight(const std::vector<FourVector>&, std::vector<double>&) const override;
+        size_t NDims() const override { return nvars; }
+
+    private:
+        RunMode mode;
+        size_t nvars;
+        double m_angle, m_lepton_energy;
+        std::shared_ptr<Beam> m_beam;
+        static constexpr double dPhi = 2*M_PI;
+        static constexpr double dCos = 2;
+        static constexpr double dp = 1000;
+};
+
+}
+
+#endif
