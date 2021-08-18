1113dc5f5142dca5ae970d34619df854fc37bc62
Joshua Isaacson <isaacson@fnal.gov>
2021-08-18 11:12:59 -0500

    Correct Sherpa Channels1

--- include/plugins/Channels1.hh
+++ include/plugins/Channels1.hh
@@ -12,7 +12,7 @@ namespace PHASIC {
 
 class Channels1 : public nuchic::Mapper<Vec4D> {
     protected:
-        constexpr static double m_amct=1.,m_alpha=0.5,m_ctmax=0.9,m_ctmin=-1.;
+        constexpr static double m_amct=1.,m_alpha=0.7,m_ctmax=1.0,m_ctmin=-1.;
         double s2, s3;
     public:
         Channels1(double _s2, double _s3) 
@@ -33,6 +33,13 @@ class C1_0 : public Channels1 {
 class C1_1 : public Channels1 {
     public:
         C1_1(double _s2, double _s3) : Channels1(_s2, _s3) {}
+        double GenerateWeight(const std::vector<Vec4D>&, std::vector<double> &) const override;
+        void GeneratePoint(std::vector<Vec4D>&, const std::vector<double> &) const override;
+};
+
+class C1_2 : public Channels1 {
+    public:
+        C1_2(double _s2, double _s3) : Channels1(_s2, _s3) {}
         double GenerateWeight(const std::vector<Vec4D>&, std::vector<double> &) const override;
         void GeneratePoint(std::vector<Vec4D>&, const std::vector<double> &) const override;
 };
