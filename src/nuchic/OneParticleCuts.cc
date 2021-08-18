b707444852d93278da887bd725387aeb6028f86f
Joshua Isaacson <isaacson@fnal.gov>
2021-08-18 10:24:50 -0500

    Refactor cut classes
    Reorganize the cuts to be a set of subclasses, with a driver class that
    holds all the cuts and evaluates them efficiently.
    
    This also made it easier to implement the two particle cuts, and any
    future needed generator level cuts. Furthermore, adding in new cuts is
    straightforward, and just requires the use of one macro function, and
    for the user to define the observable to be cut on.

---
+++ src/nuchic/OneParticleCuts.cc
@@ -0,0 +1,23 @@
+#include "nuchic/OneParticleCuts.hh"
+#include "nuchic/FourVector.hh"
+#include "nuchic/Units.hh"
+
+bool nuchic::EnergyCut::MakeCut(const FourVector &mom) const {
+    return CheckCut(mom.E());
+}
+
+bool nuchic::MomentumCut::MakeCut(const FourVector &mom) const {
+    return CheckCut(mom.P());
+}
+
+bool nuchic::AngleThetaCut::MakeCut(const FourVector &mom) const {
+    return CheckCut(mom.Theta()/1.0_deg);
+}
+
+bool nuchic::TransverseMomentumCut::MakeCut(const FourVector &mom) const {
+    return CheckCut(mom.Pt());
+}
+
+bool nuchic::ETheta2Cut::MakeCut(const FourVector &mom) const {
+    return CheckCut(mom.E()*pow(mom.Theta(), 2));
+}
