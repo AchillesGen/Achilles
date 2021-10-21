28ef857e8d350c52b59f38db6fb20df549e9b555
Joshua Isaacson <isaacson@fnal.gov>
2021-10-21 14:42:55 -0500

    Add information to write NuclearRemnant to hepmc3

--- include/nuchic/NuclearRemnant.hh
+++ include/nuchic/NuclearRemnant.hh
@@ -2,6 +2,7 @@
 #define NUCLEAR_REMNANT_HH
 
 #include "spdlog/fmt/ostr.h"
+#include "nuchic/Constants.hh"
 
 namespace nuchic {
 
@@ -15,6 +16,9 @@ class NuclearRemnant {
             os << "NuclearRemnant(" << nuclearRemnant.m_nA << ", " << nuclearRemnant.m_nZ << ")";
             return os;
         }
+
+        int PID() const { return std::stoi(fmt::format("100{:03}{:03}0", m_nZ, m_nA)); }
+        double Mass() const { return static_cast<double>(m_nA)*Constant::mN; }
 
     private:
         size_t m_nA{}, m_nZ{};
