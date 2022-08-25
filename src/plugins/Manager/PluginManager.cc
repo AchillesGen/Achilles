ea086f9dbc644d97c86e7c9300fa081d92c265ca
Joshua Isaacson <isaacson@fnal.gov>
2022-08-25 15:17:55 -0500

    Implement version checks on plugins

--- src/plugins/Manager/PluginManager.cc
+++ src/plugins/Manager/PluginManager.cc
@@ -1,37 +1,58 @@
+#include "Achilles/Utilities.hh"
+#include "Achilles/System.hh"
+#include "Achilles/Version.hh"
 #include "plugins/Manager/PluginManager.hh"
 #include "spdlog/spdlog.h"
+#include <regex>
 #include <dlfcn.h>
-#include <filesystem>
 
 using achilles::Plugin::Manager;
 
-std::vector<std::string> Manager::GetPluginPaths() {
-    std::vector<std::string> dirs;
-    const char* env = std::getenv("ACHILLES_PLUGIN_PATH");
-    if(env) achilles::tokenize(std::string(env), dirs, ":");
-    dirs.push_back(achilles::PathVariables::installLibs);
-    return dirs;
+template<typename Ret, typename... Args>
+std::function<Ret(Args...)> fptr_cast(void *fptr) {
+    using function_type = Ret(*)(Args...);
+    using ptr_size_type = std::conditional_t<sizeof(fptr) == 4, long, long long>;
+
+    return reinterpret_cast<function_type>(
+            reinterpret_cast<ptr_size_type>(fptr));
 }
 
 void Manager::Open() {
-    auto paths = GetPluginPaths();
-    for(const auto &dir : paths) {
-        std::filesystem::path directory{dir};
-        for(const auto &file_path : std::filesystem::recursive_directory_iterator(directory)) {
-            if(file_path.is_regular_file()) {
-                auto file = file_path.path().string();
-                if(file.find("AchillesPlugin") != std::string::npos
-                        && file.find(achilles::SystemVariables::libPrefix) != std::string::npos
-                        && file.find(achilles::SystemVariables::libSuffix) != std::string::npos) {
-                    void *handle = dlopen(file.c_str(), RTLD_NOW);
-                    m_handles.push_back(handle);
-                    spdlog::info("PluginManager: Loading plugin {}", file);
-                    if(!handle) {
-                        spdlog::warn("PluginManager: Could not load plugin: {}", dlerror());
-                    }
-                }
+    auto paths = achilles::Filesystem::GetPluginPaths();
+    const auto load_plugin = [&](const auto &file) {
+        auto filename = file.path().filename().string();
+        if(std::regex_search(filename, SystemVariables::validPluginName)) {
+            void *handle = dlopen(file.path().c_str(), RTLD_NOW);
+            spdlog::debug("PluginManager: Loading plugin {}", filename);
+            if(!handle) {
+                spdlog::warn("PluginManager: Could not load plugin: {}", dlerror());
+                return;
             }
+            auto get_version = fptr_cast<void, std::array<int, 3>&>(dlsym(handle, "ExpectedVersion"));
+            
+            if(dlerror() != NULL) {
+                spdlog::warn("PluginManager: Plugin does not define an Achilles version");
+                dlclose(handle); 
+                return;
+            }
+
+            std::array<int, 3> expected_version;
+            get_version(expected_version);
+            if(!ValidateVersion(expected_version)) {
+                spdlog::warn("PluginManager: Plugin expects version {}, using version {}",
+                             fmt::join(expected_version.begin(), expected_version.end(), "."),
+                             fmt::join(CurrentVersion.begin(), CurrentVersion.end(), "."));
+                spdlog::info("Ref left = {}", dlclose(handle)); 
+                return;
+            }
+
+            m_handles.push_back(std::move(handle));
         }
+    };
+
+    for(const auto &dir : paths) {
+        const auto &directory{dir};
+        Filesystem::RecurseDirectories(directory, load_plugin, achilles::Filesystem::FileType::regular);
     }
 }
 
@@ -39,4 +60,8 @@ void Manager::Close() {
     for(auto &handle : m_handles) {
         if(handle) dlclose(handle);
     }
+}
+
+bool Manager::ValidateVersion(const std::array<int, 3> &version) const {
+    return version[0] == CurrentVersion[0] && version[1] == CurrentVersion[1];
 }
