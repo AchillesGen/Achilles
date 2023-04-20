a62585161bc1fd0e2f9236557f10c5ceaaba97d1
Joshua Isaacson <isaacson@fnal.gov>
2023-04-20 16:17:34 -0500

    Resolve minor compilation issues

--- include/Achilles/Beams.hh
+++ include/Achilles/Beams.hh
@@ -129,10 +129,11 @@ class PDFBeam : public FluxType {
     public:
         PDFBeam(const YAML::Node&);
         int NVariables() const override { return 1; }
-        FourVector Flux(const std::vector<double>&) const override;
-        double GenerateWeight(const FourVector&, std::vector<double>&) const override;
+        FourVector Flux(const std::vector<double>&, double) const override;
+        double GenerateWeight(const FourVector&, std::vector<double>&, double) const override;
         std::string Type() const override { return "PDFBeam"; }
         double MaxEnergy() const override { return 0; }
+        double EvaluateFlux(const FourVector&) const override;
 
     private:
         std::unique_ptr<PDFBase> p_pdf;
