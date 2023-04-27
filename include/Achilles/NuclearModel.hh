20e799cacf18eeb097050e66397b200214d36c1f
Joshua Isaacson <isaacson@fnal.gov>
2023-04-27 00:03:17 -0500

    Handle initial state nuclear weight in NuclearModel

--- include/Achilles/NuclearModel.hh
+++ include/Achilles/NuclearModel.hh
@@ -144,6 +144,7 @@ class NuclearModel {
     std::unique_ptr<FormFactor> m_form_factor{nullptr};
 };
 
+using NuclearModelMap = std::unordered_map<NuclearMode, std::shared_ptr<NuclearModel>>;
 template <typename Derived>
 using RegistrableNuclearModel = Registrable<NuclearModel, Derived, const YAML::Node &>;
 using NuclearModelFactory = Factory<NuclearModel, const YAML::Node &>;
