aecf7b841b9f79e946761b9d7c16ee3fd9d8a187
Joshua Isaacson <isaacson@fnal.gov>
2023-04-28 10:01:03 -0500

    Get process group construction working

--- src/Achilles/Process.cc
+++ src/Achilles/Process.cc
@@ -383,6 +383,9 @@ void ProcessGroup::Optimize() {
     // TODO: Clean up this control flow. It is a bit of a mess
     b_optimize = true;
 
+    spdlog::info("Optimizing process group: Nuclear Model = {}, Multiplicity = {}",
+                 m_processes[0].Info().Multiplicity(), m_backend->GetNuclearModel()->Name());
+
     auto func = [&](const std::vector<FourVector> &mom, const double &wgt) {
         return SingleEvent(mom, wgt).Weight();
     };
