d9139d1c02fa1b19c3c09bb09a792980c22fff3d
Stefan Hoeche <shoeche@fnal.gov>
2023-04-28 10:06:58 -0500

    added sherpa form factor interface

---
+++ include/plugins/Sherpa/FormFactors.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "METOOLS/Explicit/Form_Factor.H"
+#include "METOOLS/Explicit/Vertex_Key.H"
+
+#include "Achilles/FormFactor.hh"
+#include "Achilles/Units.hh"
+
+namespace achilles {
+
+  class FormFactorInterface: public METOOLS::Form_Factor {
+  private:
+    static std::unique_ptr<FormFactor> p_ff;
+    std::string m_id;
+    int m_mode;
+  public:
+    FormFactorInterface(const METOOLS::Vertex_Key &key,
+			const std::string &id);
+    double FF();
+    inline void SetFormFactor(std::unique_ptr<FormFactor> ff)
+    { p_ff=std::move(ff); }
+  };// end of class FormFactorInterface
+
+}// end of namespace achilles
