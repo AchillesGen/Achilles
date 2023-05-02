5805ac4cec6234ac1d92d0bcb08dbc84ddb0f1a5
Joshua Isaacson <isaacson@fnal.gov>
2023-05-02 00:11:30 -0500

    Reorder Ids in ProcesSInfo

--- src/Achilles/ProcessInfo.cc
+++ src/Achilles/ProcessInfo.cc
@@ -12,13 +12,13 @@ size_t achilles::ProcessInfo::NInitialStates(size_t nprotons, size_t nneutrons) 
 std::vector<double> achilles::ProcessInfo::Masses() const {
     std::vector<double> masses;
 
-    // Get Hadronic current masses
-    for(const auto &part : m_hadronic.second) {
+    // Get Leptonic current masses
+    for(const auto &part : m_leptonic.second) {
         masses.push_back(pow(ParticleInfo(part).Mass(), 2));
     }
 
-    // Get Leptonic current masses
-    for(const auto &part : m_leptonic.second) {
+    // Get Hadronic current masses
+    for(const auto &part : m_hadronic.second) {
         masses.push_back(pow(ParticleInfo(part).Mass(), 2));
     }
 
@@ -28,17 +28,17 @@ std::vector<double> achilles::ProcessInfo::Masses() const {
 std::vector<long> achilles::ProcessInfo::Ids() const {
     std::vector<long> ids;
 
-    // Get initial hadronic ids
-    for(const auto &part : m_hadronic.first) ids.push_back(part.AsInt());
-
     // Get inital lepton id
     ids.push_back(m_leptonic.first.AsInt());
 
-    // Get remaining hadronic ids
-    for(const auto &part : m_hadronic.second) ids.push_back(part.AsInt());
+    // Get initial hadronic ids
+    for(const auto &part : m_hadronic.first) ids.push_back(part.AsInt());
 
     // Get remaining leptonic ids
     for(const auto &part : m_leptonic.second) ids.push_back(part.AsInt());
+
+    // Get remaining hadronic ids
+    for(const auto &part : m_hadronic.second) ids.push_back(part.AsInt());
 
     return ids;
 }
