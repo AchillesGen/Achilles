05635d322833625735dff3f0735c35853543f2fa
Joshua Isaacson <isaacson@fnal.gov>
2023-05-02 00:07:18 -0500

    Simplify Event class with refactoring

--- src/Achilles/Event.cc
+++ src/Achilles/Event.cc
@@ -7,62 +7,16 @@
 using achilles::Event;
 
 Event::Event(std::shared_ptr<Nucleus> nuc, std::vector<FourVector> mom, double vwgt)
-    : m_nuc{std::move(nuc)}, m_mom{std::move(mom)}, m_vWgt{std::move(vwgt)} {
+    : m_nuc{std::move(nuc)}, m_mom{std::move(mom)}, m_wgt{std::move(vwgt)} {
     m_nuc->GenerateConfig();
     m_me.resize(m_nuc->NNucleons());
-}
-
-// bool Event::ValidateEvent(size_t imatrix) const {
-//     spdlog::trace("Number of momentums = {}", m_mom.size());
-//     spdlog::trace("Number of states = {}", m_me[imatrix].inital_state.size()
-//     + m_me[imatrix].final_state.size()); return m_mom.size() ==
-//     m_me[imatrix].inital_state.size() + m_me[imatrix].final_state.size();
-// }
-
-// TODO: Make this cleaner and handle multiple nucleons in initial state
-void Event::InitializeLeptons(const ProcessInfo &process) {
-    // Setup leptons
-    bool initial_state = true;
-    for(const auto &elm : process.m_mom_map) {
-        if(ParticleInfo(elm.second).IsLepton() || ParticleInfo(elm.second).IsVector()) {
-            m_leptons.emplace_back(ParticleInfo(elm.second), m_mom[elm.first]);
-            if(initial_state) {
-                m_leptons.back().Status() = ParticleStatus::initial_state;
-                initial_state = false;
-            } else {
-                m_leptons.back().Status() = ParticleStatus::final_state;
-            }
-        }
-    }
-}
-
-void Event::InitializeHadrons(const ProcessInfo &process) {
-    // Coherent scattering is handled inside the coherent class
-    if(ParticleInfo(process.m_hadronic.first[0]).IsNucleus()) return;
-
-    // Get all hadronic momenta
-    std::vector<FourVector> mom;
-    for(const auto &elm : process.m_mom_map) {
-        if(ParticleInfo(elm.second).IsHadron()) { mom.push_back(m_mom[elm.first]); }
-    }
-
-    // TODO: Refactor to use the process / process_group structure
-    // Initial state setup
-    size_t idx = SelectNucleon();
-    Particle &initial = m_nuc->Nucleons()[idx];
-    initial.Momentum() = mom.front();
-    initial.Status() = ParticleStatus::initial_state;
-
-    // Final state setup
-    Particle final(process.m_hadronic.second[0], mom.back(), initial.Position(),
-                   ParticleStatus::propagating);
-    m_nuc->Nucleons().push_back(final);
 }
 
 void Event::Finalize() {
     size_t nA = 0, nZ = 0;
     for(auto it = m_nuc->Nucleons().begin(); it != m_nuc->Nucleons().end();) {
-        if(it->Status() == ParticleStatus::background) {
+        if(it->Status() == ParticleStatus::background ||
+           it->Status() == ParticleStatus::propagating) {
             if(it->ID() == PID::proton()) nZ++;
             nA++;
             it = m_nuc->Nucleons().erase(it);
@@ -72,6 +26,16 @@ void Event::Finalize() {
     }
 
     m_remnant = NuclearRemnant(nA, nZ);
+}
+
+void Event::Display() const {
+    spdlog::trace("Leptons:");
+    size_t idx = 0;
+    for(const auto &particle : Leptons()) { spdlog::trace("\t{}: {}", ++idx, particle); }
+    spdlog::trace("Hadrons:");
+    idx = 0;
+    for(const auto &particle : Hadrons()) { spdlog::trace("\t{}: {}", ++idx, particle); }
+    spdlog::trace("Weight: {}", Weight());
 }
 
 achilles::vParticles Event::Particles() const {
@@ -87,38 +51,6 @@ const achilles::vParticles &Event::Hadrons() const {
 
 achilles::vParticles &Event::Hadrons() {
     return m_nuc->Nucleons();
-}
-
-void Event::CalcWeight() {
-    // if(!ValidateEvent(0))
-    //     throw std::runtime_error("Phase space and Matrix element have
-    //     different number of particles");
-    m_wgt = m_vWgt * m_meWgt;
-}
-
-bool Event::TotalCrossSection() {
-    m_meWgt = std::accumulate(m_me.begin(), m_me.end(), 0.0, std::plus<>());
-    spdlog::debug("Total xsec = {}", m_meWgt);
-    return m_meWgt > 0 ? true : false;
-}
-
-size_t Event::SelectNucleon() const {
-    std::vector<double> probs = EventProbs();
-    double rand = Random::Instance().Uniform(0.0, 1.0);
-    return static_cast<size_t>(
-               std::distance(probs.begin(), std::lower_bound(probs.begin(), probs.end(), rand))) -
-           1;
-}
-
-std::vector<double> Event::EventProbs() const {
-    std::vector<double> probs;
-    probs.push_back(0.0);
-    double cumulative = 0.0;
-    for(const auto &m : m_me) {
-        cumulative += m;
-        probs.emplace_back(cumulative / m_meWgt);
-    }
-    return probs;
 }
 
 void Event::Rotate(const std::array<double, 9> &rot_mat) {
