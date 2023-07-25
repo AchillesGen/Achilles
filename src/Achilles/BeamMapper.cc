21bbc92d6f0e0fd6a926318bf8562e330aa63900
pmachado85 <pmachado@fnal.gov>
2023-07-25 17:05:23 -0500

    fix beam minimum energy and number of channels

--- src/Achilles/BeamMapper.cc
+++ src/Achilles/BeamMapper.cc
@@ -7,21 +7,19 @@ using achilles::BeamMapper;
 void BeamMapper::GeneratePoint(std::vector<FourVector> &point, const std::vector<double> &rans) {
     auto beam_id = *m_beam->BeamIDs().begin();
     // TODO: 1. Resolve this arbitrary eps with a cut
-    //       2. Replace the sqrt(Smin())-sqrt(Masses()[0]) with final state
-    //       hadronic mass
-    static constexpr double eps = 5;
-    point[m_idx] = m_beam->Flux(beam_id, rans, sqrt(Smin()) - sqrt(Masses()[0]) + eps);
+    //       2. Replace the sqrt(Smin())-sqrt(Masses()[0]) with final state hadronic mass
+    // static constexpr double eps=5;
+    point[m_idx] = m_beam->Flux(beam_id, rans, (Smin() - Masses()[0]) / (2 * sqrt(Masses()[0])));
     Mapper<FourVector>::Print(__PRETTY_FUNCTION__, point, rans);
 }
 
 double BeamMapper::GenerateWeight(const std::vector<FourVector> &point, std::vector<double> &rans) {
     auto beam_id = *m_beam->BeamIDs().begin();
     // TODO: 1. Resolve this arbitrary eps with a cut
-    //       2. Replace the sqrt(Smin())-sqrt(Masses()[0]) with final state
-    //       hadronic mass
-    static constexpr double eps = 5;
-    auto wgt =
-        m_beam->GenerateWeight(beam_id, point[m_idx], rans, sqrt(Smin()) - sqrt(Masses()[0]) + eps);
+    //       2. Replace the sqrt(Smin())-sqrt(Masses()[0]) with final state hadronic mass
+    // static constexpr double eps=5;
+    auto wgt = m_beam->GenerateWeight(beam_id, point[m_idx], rans,
+                                      (Smin() - Masses()[0]) / (2 * sqrt(Masses()[0])));
     Mapper<FourVector>::Print(__PRETTY_FUNCTION__, point, rans);
     spdlog::trace("  Beam weight = {}", wgt);
     return 1.0 / wgt;
