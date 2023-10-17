c9e325acb3eade660eab80ba6ad531758161d2ab
Joshua Isaacson <isaacson@fnal.gov>
2023-10-17 15:09:20 -0400

    Quick patch for ROOT histogram flux normalization issue

--- src/Achilles/Beams.cc
+++ src/Achilles/Beams.cc
@@ -167,7 +167,8 @@ Spectrum::Spectrum(const YAML::Node &node) {
         TFile *file = new TFile(filename.c_str());
         TH1D *hist =
             static_cast<TH1D *>(file->Get(node["ROOTHist"]["HistName"].as<std::string>().c_str()));
-        bool use_width = node["ROOTHist"]["UseWidth"].as<bool>();
+        // BUG: Using widths here for the normalization of the integral breaks the normalization
+        // when dividing the bin widths bool use_width = node["ROOTHist"]["UseWidth"].as<bool>();
         double norm = node["ROOTHist"]["Norm"].as<double>();
         hist->Scale(norm);
         std::vector<double> bin_centers; //(static_cast<size_t>(hist -> GetNbinsX())+2);
