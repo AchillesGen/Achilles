55cedfe6b9da328b626e22128c95a50c89e7d534
Noah Steinberg <nastein@umich.edu>
2024-04-12 12:03:41 -0400

    Next computation of 1 body and 2 body current from interference model switches upon calculating the current

--- src/Achilles/Process.cc
+++ src/Achilles/Process.cc
@@ -270,7 +270,6 @@ void ProcessGroup::SetupLeptons(Event &event, std::optional<size_t> process_idx)
     process.ExtractMomentum(event, lep_in, had_in, lep_out, had_out, spect);
     std::vector<Particle> leptons;
 
-    spdlog::trace("settig up leptons2");
     // Setup leptons
     const auto &info = process.Info();
     leptons.emplace_back(info.m_leptonic.first, lep_in);
@@ -283,7 +282,6 @@ void ProcessGroup::SetupLeptons(Event &event, std::optional<size_t> process_idx)
         leptons.back().Status() = ParticleStatus::final_state;
     }
 
-    spdlog::trace("settig up leptons3");
     event.Leptons() = leptons;
 }
 
