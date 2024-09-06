f4e9c823c1f5f3d13d95830a43e2944b61f57d44
Joshua Isaacson <isaacson@fnal.gov>
2024-09-06 16:52:54 -0400

    feat: Implement Reference handling and output

--- test/mock_classes.hh
+++ test/mock_classes.hh
@@ -70,7 +70,6 @@ class MockDensity : public trompeloeil::mock_interface<achilles::Density> {
 class MockPotential : public trompeloeil::mock_interface<achilles::Potential> {
     static constexpr bool trompeloeil_movable_mock = true;
     IMPLEMENT_CONST_MOCK2(Hamiltonian);
-    IMPLEMENT_CONST_MOCK0(GetReference);
     achilles::PotentialVals operator()(double p, double r) const override { return call_op(p, r); }
     MAKE_CONST_MOCK2(call_op, achilles::PotentialVals(const double &, const double &));
 };
