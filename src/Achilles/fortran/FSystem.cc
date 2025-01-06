33ca9b13c6daa070a2b589f8c326234c99a86b48
Joshua Isaacson <isaacson@fnal.gov>
2025-01-06 16:18:47 -0500

    feat: Provide fortran interface to FindFile

---
+++ src/Achilles/fortran/FSystem.cc
@@ -0,0 +1,15 @@
+#include "Achilles/System.hh"
+
+extern "C" {
+
+char *FindFile(char *cfilename, char *chead) {
+    std::string filename(cfilename);
+    std::string head(chead);
+
+    std::string result = achilles::Filesystem::FindFile(filename, head);
+    char *cresult = new char[result.size()];
+    strcpy(cresult, result.c_str());
+    return cresult;
+}
+
+}
---
+++ src/Achilles/fortran/system_mod.f90
@@ -0,0 +1,33 @@
+module libsystem
+
+    use, intrinsic :: iso_c_binding
+    implicit none
+
+    private
+
+    interface
+        function find_file_c(file, head) bind(C, name="FindFile")
+            use iso_c_binding
+            implicit none
+            type(c_ptr), value :: file, head
+            type(c_ptr) :: find_file_c
+        end function
+    end interface
+
+    public :: find_file
+
+contains
+    function find_file(file, head)
+        use libutilities
+        implicit none
+        type(c_ptr) :: cfile, chead
+        character(len=*), intent(in) :: file, head
+        character(len=:), allocatable :: find_file
+
+        cfile = f2cstring(file)
+        chead = f2cstring(head)
+        find_file = c2fstring(find_file_c(cfile, chead))
+        call free(cfile)
+        call free(chead)
+    end function
+end module
