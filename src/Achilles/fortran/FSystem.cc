1ef44d35bdb30ed28749a49ccc0bfbb789198039
Joshua Isaacson <isaacson@fnal.gov>
2025-01-07 15:57:42 -0500

    build: Attempt using std::copy instead of strcpy

--- src/Achilles/fortran/FSystem.cc
+++ src/Achilles/fortran/FSystem.cc
@@ -9,8 +9,8 @@ char *FindFile(char *cfilename, char *chead) {
     spdlog::info("Looking for file: {} abc", filename);
     std::string result = achilles::Filesystem::FindFile(filename, head);
     spdlog::info("Found file at: {} abc", result);
-    char *cresult = new char[result.size()];
-    strcpy(cresult, result.c_str());
+    char *cresult = new char[result.size()+1];
+    std::copy(result.begin(), result.end(), cresult);
     return cresult;
 }
 }
