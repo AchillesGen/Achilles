c288a83aa82695fb06071e524a53e3fb2cfab775
Joshua Isaacson <isaacson@fnal.gov>
2025-01-07 16:13:43 -0500

    build: Remove debug prints

--- src/Achilles/fortran/FSystem.cc
+++ src/Achilles/fortran/FSystem.cc
@@ -6,9 +6,7 @@ char *FindFile(char *cfilename, char *chead) {
     std::string filename(cfilename);
     std::string head(chead);
 
-    spdlog::info("Looking for file: {} abc", filename);
     std::string result = achilles::Filesystem::FindFile(filename, head);
-    spdlog::info("Found file at: {} abc", result);
     char *cresult = new char[result.size() + 1];
     std::copy(result.begin(), result.end(), cresult);
     return cresult;
