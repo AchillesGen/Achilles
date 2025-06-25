1922d5c734e48258c12fe0df6126a1859ef9b21a
Noah Steinberg <nastein@umich.edu>
2025-06-25 18:04:01 -0500

    DeltaInteractions and NN now take exponential suppression exponent as parameter

--- src/Achilles/CascadeInteractions/DeltaInteractions.cc
+++ src/Achilles/CascadeInteractions/DeltaInteractions.cc
@@ -94,7 +94,6 @@ InteractionResults DeltaInteraction::CrossSection(Event &event, size_t part1, si
     // NOTE: Particle 2 is always a nucleon in the cascade algorithm
     if(particle1.Info().IsResonance()) {
 
-        
         // NDelta -> NN
         double xsec = suppression * SigmaNDelta2NN(sqrts, p1CM, particle1.ID(), particle2.ID(),
                                                    particle1.Momentum().M());
