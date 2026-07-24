258f0070ce512b0079a0b02230ad2a3cc9b74181
Josh Isaacson <isaacs21@msu.edu>
2026-07-24 14:44:23 -0400

    fix: Resolve install issues for NuGeom

--- CMake/ProgramOptions.cmake
+++ CMake/ProgramOptions.cmake
@@ -4,9 +4,8 @@ option(ACHILLES_ENABLE_POTENTIAL_TEST "Enable executables for testing the potent
 option(ACHILLES_ENABLE_PRECOMPUTED "Enable executables for running the cascade on precomputed events" OFF)
 
 # Achilles interface to optional external libraries
-option(ACHILLES_ENABLE_SHERPA "Enable the generation of events using Sherpa" OFF) 
+option(ACHILLES_ENABLE_SHERPA "Enable the generation of events using Sherpa" OFF)
 option(ACHILLES_ENABLE_ROOT "Enable reading of ROOT flux files" OFF)
-SET(ACHILLES_ENABLE_HEPMC3 TRUE)
 
 # BUild options
 option(BUILD_SHARED_LIBS "Enable compilation of shared libraries" ON)
